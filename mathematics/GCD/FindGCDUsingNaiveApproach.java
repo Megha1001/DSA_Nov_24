3fb3337e03deace5ff9a4c8949a5c217c504276c
Megha Verma <meghaverma1001@gmail.com>
2024-11-03 12:49:46 +0530

    Update FindGCDUsingNaiveApproach.java

--- mathematics/GCD/FindGCDUsingNaiveApproach.java
+++ mathematics/GCD/FindGCDUsingNaiveApproach.java
@@ -4,7 +4,7 @@ package mathematics.GCD;
  * GCD stands for greatest common divisor, basically it is the largest number that divides both the number.
  * IF GCD is 1 that doesn't mean number are prime. they can be composite like 9,10
  * 
- * TIME COMPLEXITY : Theta(min(a,b))
+ * TIME COMPLEXITY : O(min(a,b)), O since we can find the divisor prior to all the iterations
  * AUXILIART SPACE: Theta(1)
  *
  */
