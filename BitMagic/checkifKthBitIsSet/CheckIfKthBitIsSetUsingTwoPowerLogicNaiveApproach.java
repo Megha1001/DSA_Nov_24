fcc8772f3a38a4d571a54580c42149818bb7bddc
Megha Verma <meghaverma1001@gmail.com>
2024-11-04 21:51:24 +0530

    Create CheckIfKthBitIsSetUsingTwoPowerLogicNaiveApproach.java

---
+++ BitMagic/checkifKthBitIsSet/CheckIfKthBitIsSetUsingTwoPowerLogicNaiveApproach.java
@@ -0,0 +1,32 @@
+package BitMagic.checkifKthBitIsSet;
+
+/*
+ * Logic : 
+ * 1. If we do bitwise & with 2^k-1 and if that result !=0 then kth bit is set otherwise not set.
+ * 
+ * TIME COMPLEXITY : Theta(K)
+ * AUXILIARY SPACE : Theta(1)
+ */
+
+public class CheckIfKthBitIsSetUsingTwoPowerLogicNaiveApproach {
+   
+    public static void main(String args[]){
+        int n =5, k=1;
+        // int n =8, k=2;
+        // int n =0, k=3;
+        System.out.println("is the kth bit of given number is set ? "+checkIfKthBitIsSet(n, k));
+    }
+    
+
+    public static boolean checkIfKthBitIsSet(int n, int k){
+       int x = 1;
+
+       for(int i=0; i<(k-1); i++){
+            x = x*2;
+       }
+
+       return (n&x)!=0;
+
+    }
+
+}
