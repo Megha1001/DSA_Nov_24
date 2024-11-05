928a2591085e24352f859a99de6c3b183d28a66d
Megha Verma <meghaverma1001@gmail.com>
2024-11-05 22:41:36 +0530

    Create FindTwoOddOccurringNumberNaive.java

---
+++ BitMagic/twoOddOccurring/FindTwoOddOccurringNumberNaive.java
@@ -0,0 +1,13 @@
+package BitMagic.twoOddOccurring;
+
+
+/*
+ * LOGIC : Traverse through the array , count occurrence of every number. If number is odd, print the number.
+ * 
+ * TIME COMPLEXITY : Theta(N^2)
+ * AUXILIARY SPACE : Theta(1)
+ */
+
+public class FindTwoOddOccurringNumberNaive {
+    
+}
