ae9a2f677a5a8672cb48435c1d8df0f18aa1ffcb
Megha Verma <meghaverma1001@gmail.com>
2024-11-05 22:44:59 +0530

    Update FindTwoOddOccurringNumberNaive.java

--- BitMagic/twoOddOccurring/FindTwoOddOccurringNumberNaive.java
+++ BitMagic/twoOddOccurring/FindTwoOddOccurringNumberNaive.java
@@ -9,5 +9,27 @@ package BitMagic.twoOddOccurring;
  */
 
 public class FindTwoOddOccurringNumberNaive {
+
+    public static void main(String args[]){
+        int arr[] = {3,4,5,5,5,5};
+        System.out.println("The numbers that are occurring odd times are : ");
+        printOddOccurrence(arr);
+    }
+
+    public static void printOddOccurrence(int arr[]){
+        int len = arr.length;
+        for(int i=0; i<len; i++){
+            int count=0;
+            for(int j=0; j<len; j++){
+                if(arr[i]==arr[j]){
+                    ++count;
+                }
+            }
+            if(count%2!=0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+
+    }
     
 }
