e1d9b9f8d0fe82c1c60a730a67f2765ce5da6178
Megha Verma <meghaverma1001@gmail.com>
2024-11-06 21:42:39 +0530

    Update FindTwoOddOccurringNumberUsingXOR.java

--- BitMagic/twoOddOccurring/FindTwoOddOccurringNumberUsingXOR.java
+++ BitMagic/twoOddOccurring/FindTwoOddOccurringNumberUsingXOR.java
@@ -17,7 +17,9 @@ package BitMagic.twoOddOccurring;
 public class FindTwoOddOccurringNumberUsingXOR {
 
     public static void main(String args[]){
-        int arr[] = {5,6,10,6,10,6,3,3};
+        // int arr[] = {5,6,10,6,10,6,3,3};
+        // int arr[] = {0,1,1,6,2,2};
+        int arr[] = {0,8,1,1,2,2};
         System.out.println(" The two times odd occurring numbers in given data are : ");
         int res[] = findTwoOddOccurringNumber(arr);
         System.out.print(res[0]+","+res[1]);
@@ -38,30 +40,37 @@ public class FindTwoOddOccurringNumberUsingXOR {
         //find SetBit in XORed number
         int setBit = findSetBitInXoredNum(xor); // setbit finding is done on zero index basis
 
-        //divide into two group
-        int firstGrp [] = new int[len];
-        int idx1=0;
-        int secondGrp [] = new int[len];
-        int idx2=0;
+        // //divide into two group
+        // int firstGrp [] = new int[len];
+        // int idx1=0;
+        // int secondGrp [] = new int[len];
+        // int idx2=0;
 
-        for(int i=0; i<len; i++){
-            //bit is set
-            if(((1<<(setBit))& arr[i])> 0){
-                firstGrp[idx1] = arr[i];
-                ++idx1;
-            }else{
-                secondGrp[idx2] = arr[i];
-                ++idx2; 
-            }
-        }
+        // for(int i=0; i<len; i++){
+        //     //bit is set
+        //     if(((1<<(setBit))& arr[i])> 0){
+        //         firstGrp[idx1] = arr[i];
+        //         ++idx1;
+        //     }else{
+        //         secondGrp[idx2] = arr[i];
+        //         ++idx2; 
+        //     }
+        // }
         
         //XORed the items in two groups seperately
-        res[0] = firstGrp[0];
-        res[1] = secondGrp[0];
+        // res[0] = firstGrp[0];
+        // res[1] = secondGrp[0];
 
-        for(int i=1; i<len; i++){
-            res[0] ^= firstGrp[i];
-            res[1] ^= secondGrp[i];
+        // for(int i=1; i<len; i++){
+        //     res[0] ^= firstGrp[i];
+        //     res[1] ^= secondGrp[i];
+        // }
+        for(int i=0;i<len;i++){
+            if((arr[i]& setBit)!=0){
+                res[0] ^= arr[i];
+            }else{
+                res[1] ^= arr[i];
+            }
         }
 
         return res;
@@ -72,7 +81,8 @@ public class FindTwoOddOccurringNumberUsingXOR {
         //assumption 32 bit number
         for(int i=0 ;i<31;i++){
             if(((1<<i)&xor) >0){
-                return i;
+                // return i;
+                return i+1;
             }
         }
         return -1;
