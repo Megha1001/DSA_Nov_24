b0894b81e9ad7a3761d339b1c4c0ff3be8c2394f
Megha Verma <meghaverma1001@gmail.com>
2024-11-07 23:39:49 +0530

    Create FindFactorialUsingRecursion.java

---
+++ recursion/factorial/FindFactorialUsingRecursion.java
@@ -0,0 +1,21 @@
+package recursion.factorial;
+
+/*
+ * LOGIC : Using tail recursion / tail call elimination
+ * 
+ * TIME COMPLEXITY : O(N)
+ * AUXILIARY SPACE : Theta(1)
+ */
+
+public class FindFactorialUsingRecursion {
+
+    public static void main(String args[]){
+        int number = 5;
+        System.out.println("Factorial of the given number using recursion is : "+findFac(number, 1));
+    }
+
+    public static int findFac(int n, int k){
+        
+    }
+    
+}
