fd78fc699470321010c1d03bfa62290f68bfc382
Megha Verma <meghaverma1001@gmail.com>
2024-11-08 23:36:21 +0530

    Update FindPalindromeUsingRecursionOptimized.java

--- recursion/palindrome/FindPalindromeUsingRecursionOptimized.java
+++ recursion/palindrome/FindPalindromeUsingRecursionOptimized.java
@@ -1,5 +1,11 @@
 package recursion.palindrome;
 
+
+/*
+ * TIME COMPLEXITY : O(N), specifically N/2
+ * AUXILIARY SPACE : O(N) , specifically N/2
+ */
+
 public class FindPalindromeUsingRecursionOptimized {
 
     public static void main(String args[]){
@@ -9,7 +15,7 @@ public class FindPalindromeUsingRecursionOptimized {
         System.out.println("is the given number palindrome : ?"+isPalindrome(input, startIdx, endIdx));
     }
 
-    public static boolean isPalindrome(int inpu, int startIdx, int endIdx){
+    public static boolean isPalindrome(String input, int startIdx, int endIdx){
 
         // base case -> n==0 || n==1 return true, n becomes zero when string length is even and n becomes 1 when string length is odd.
         /*
@@ -17,10 +23,10 @@ public class FindPalindromeUsingRecursionOptimized {
          * startIdx >= endIdx , when string length is even
          */
         if(startIdx >= endIdx){
-
+            return true;
         }
 
-
+        return (input.charAt(startIdx) == input.charAt(endIdx)) && isPalindrome(input, startIdx+1, endIdx-1);
     }
     
 }
