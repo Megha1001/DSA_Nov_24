852fb58da05b1c86ad3f7c6ea594d2bb8552a001
Megha Verma <meghaverma1001@gmail.com>
2024-11-09 01:14:01 +0530

    Create RopeCuttingProblemNaiveApproach.java

---
+++ recursion/ropeCuttingProblem/RopeCuttingProblemNaiveApproach.java
@@ -0,0 +1,35 @@
+package recursion.ropeCuttingProblem;
+
+public class RopeCuttingProblemNaiveApproach {
+
+    public static void main(String args[]){
+        // int n=17,  a=10, b=11, c=3; o/p : 3
+        int n=10,  a=9, b=7, c=11; //o/p : -1
+
+        System.out.println("Max pieces that can be cut from rop of length N is : "+maxPieces(n, a, b, c));
+    }
+
+    public static int maxPieces(int n, int a, int b, int c){
+
+        //base case
+        if(n==0){
+            return 0;
+        }
+
+        //base case
+        if(n<=-1){
+            return -1;
+        }
+
+        int res = Math.max(maxPieces(n-a, a, b, c), Math.max(maxPieces(n-b, a, b, c), maxPieces(n-c, a, b, c)));
+
+        //important condition
+        if(res==-1){
+            return -1;
+        }
+
+        return res+1;
+
+    }
+    
+}
