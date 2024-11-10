ffedc12a9c46b188c41fd621468e8c9b5e20a38d
Megha Verma <meghaverma1001@gmail.com>
2024-11-10 10:59:33 +0530

    Create JosephusProblem.java

---
+++ recursion/josephusProblem/JosephusProblem.java
@@ -0,0 +1,14 @@
+package recursion.josephusProblem;
+
+/*
+ * Problem Statement : There are n people standing in a circle waiting to be executed. The counting out begins at some point in the circle and proceeds around the circle in a fixed direction.
+ * In each step, a certain number of people are skipped and the next person is executed. The elimination proceeds around the circle(which is becoming smaller and smaller as the exectued people are removed),
+ * until only the last person remains, who is given freedom.
+ * 
+ * Given the total number of persons n, and a number k which indicates that k-1 perosn s are skipped and kth person is killed in a circle. The task is to choose the place in the intial circle so that you are the last one remaining
+ * and so survive.
+ */
+
+public class JosephusProblem {
+    
+}
