504c6ee70e45848d5c7e667e240e3ec91f61c57d
Megha Verma <meghaverma1001@gmail.com>
2024-11-11 23:36:48 +0530

    Update ReverseAnArrayNaiveApproach.java

--- arrays/reverseAnArray/ReverseAnArrayNaiveApproach.java
+++ arrays/reverseAnArray/ReverseAnArrayNaiveApproach.java
@@ -13,7 +13,7 @@ public class ReverseAnArrayNaiveApproach {
 
         System.out.println("Arrays after getting reverse sorted");
         reverseSort(arr, arr.length);
-        reverseSort(arr1, arr1.length);
+        reverse(arr1, arr1.length);
     }
 
     public static void reverseSort(int arr[], int n){
@@ -33,6 +33,26 @@ public class ReverseAnArrayNaiveApproach {
         }
 
         System.out.println();
+    }
+
+    /*
+     * Easy way to write the same code
+     */
+
+    public static void reverse (int arr[], int n){
+        int low = 0;
+        int high = n-1;
+        while(low < high){
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            ++low;
+            --high;
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+
     }
 
 }
