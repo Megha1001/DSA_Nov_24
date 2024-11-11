c7abdf7c3dd09987234415fe4516fdb6b26281be
Megha Verma <meghaverma1001@gmail.com>
2024-11-11 23:10:32 +0530

    Update CheckIfArrayIsNaturalSortedEfficientApproach.java

--- arrays/checkArrayIsSorted/CheckIfArrayIsNaturalSortedEfficientApproach.java
+++ arrays/checkArrayIsSorted/CheckIfArrayIsNaturalSortedEfficientApproach.java
@@ -12,4 +12,15 @@ public class CheckIfArrayIsNaturalSortedEfficientApproach {
 
     }
 
+    public static boolean checkIfSorted(int arr[], int n){
+
+        for(int i=1; i<n; i++){
+            if(arr[i]<arr[i-1]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
