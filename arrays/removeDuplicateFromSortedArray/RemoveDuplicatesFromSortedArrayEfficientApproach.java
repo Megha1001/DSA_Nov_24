c2be801ec37b523df032d1f1ddebb1e95fbed846
Megha Verma <meghaverma1001@gmail.com>
2024-11-12 00:11:27 +0530

    Update RemoveDuplicatesFromSortedArrayEfficientApproach.java

--- arrays/removeDuplicateFromSortedArray/RemoveDuplicatesFromSortedArrayEfficientApproach.java
+++ arrays/removeDuplicateFromSortedArray/RemoveDuplicatesFromSortedArrayEfficientApproach.java
@@ -5,11 +5,14 @@ public class RemoveDuplicatesFromSortedArrayEfficientApproach {
     public static void main(String args[]){
         int arr[] = {1,4,5,6,7,7,7,8,8,8,9,10};
         System.out.println("Array after removing duplicates is : ");
-        removeDuplicates(arr, arr.length);
+        int uniqueElements = removeDuplicates(arr, arr.length);
+        for(int i=0; i<uniqueElements; i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 
 
-    public static void removeDuplicates(int arr[], int n){
+    public static int removeDuplicates(int arr[], int n){
 
         int idx = 1;
         for(int i=1; i<n; i++){
@@ -19,10 +22,7 @@ public class RemoveDuplicatesFromSortedArrayEfficientApproach {
             }
         }
 
-        //printing
-        for(int i=0; i<n; i++){
-            System.out.print(arr[i]+" ");
-        }
+        return idx;
 
     }
 
