eb826ce81a7b11296868cbce66e2d092ca386b38
Megha Verma <meghaverma1001@gmail.com>
2024-11-11 23:49:20 +0530

    Create RemoveDuplicatedFromSortedArrayNaiveApproarch.java

---
+++ arrays/removeDuplicateFromSortedArray.java/RemoveDuplicatedFromSortedArrayNaiveApproarch.java
@@ -0,0 +1,11 @@
+package arrays.removeDuplicateFromSortedArray.java;
+
+public class RemoveDuplicatedFromSortedArrayNaiveApproarch {
+
+    public static void main(String args[]){
+        int arr[] = {1,4,5,6,7,7,7,8,8,8,9,10};
+        System.out.println("Array after removing duplicates is : ");
+        removeDuplicates()
+    }
+    
+}
