721661ba6922fb899930dd6a1cae1b1f7c7e5a8e
Megha Verma <meghaverma1001@gmail.com>
2024-11-11 23:26:39 +0530

    Create ReverseAnArrayNaiveApproach.java

---
+++ arrays/reverseAnArray/ReverseAnArrayNaiveApproach.java
@@ -0,0 +1,31 @@
+package arrays.reverseAnArray;
+
+public class ReverseAnArrayNaiveApproach {
+    
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5}; //odd size
+        int arr1[] = {1,2,3,4,5,6}; //even size
+
+        System.out.println("Arrays after getting reverse sorted");
+        reverseSort(arr, arr.length);
+        reverseSort(arr1, arr1.length);
+    }
+
+    public static void reverseSort(int arr[], int n){
+        int start = 0;
+        int end = n-1;
+        int mid = (start+end)/2;
+
+        for(int i=0; i<=mid; i++){
+            int temp = arr[i];
+            arr[i] = arr[n-1-i];
+            arr[n-1-i] = temp;
+        }
+
+        for(int i=0; i<n; i++){
+            System.out.print()
+        }
+
+    }
+
+}
