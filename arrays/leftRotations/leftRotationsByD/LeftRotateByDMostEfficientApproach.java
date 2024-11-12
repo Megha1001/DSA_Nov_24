6b4fbbfdd67d45d9e3847576e20fbb9b813baeff
Megha Verma <meghaverma1001@gmail.com>
2024-11-12 23:20:06 +0530

    Update LeftRotateByDMostEfficientApproach.java

--- arrays/leftRotations/leftRotationsByD/LeftRotateByDMostEfficientApproach.java
+++ arrays/leftRotations/leftRotationsByD/LeftRotateByDMostEfficientApproach.java
@@ -1,5 +1,12 @@
 package arrays.leftRotations.leftRotationsByD;
 
+/*
+ * LOGIC
+ *  -reverse(arr, 0,d-1)
+ *  -reverse(arr, d, n-1)
+ *  -reverse(arr, 0, n-1)
+ */
+
 public class LeftRotateByDMostEfficientApproach {
 
     public static void main(String args[]){
