ebfa70778ee7088074d571eb2077c07a496b3aee
Megha Verma <meghaverma1001@gmail.com>
2024-11-12 22:50:18 +0530

    Create LeftRotateByDNaiveApproach.java

---
+++ arrays/leftRotations/leftRotationsByD/LeftRotateByDNaiveApproach.java
@@ -0,0 +1,40 @@
+package arrays.leftRotations.leftRotationsByD;
+
+
+/*
+ * LOGIC : Call leftRotate by 1 d times
+ * 
+ * TIME 
+ */
+
+public class LeftRotateByDNaiveApproach {
+
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5,6};
+        int d=3;
+        System.out.println("Array after "+d+" rotation is : ");
+        rotateArrayByDPosition(arr, arr.length, d);
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void rotateArrayByDPosition(int arr[], int n, int d){
+        int count=0;
+        while(count!=d){
+            rotateByOnePlace(arr, n);
+            ++count;
+        }
+    }
+
+    public static void rotateByOnePlace(int arr[], int n){
+        int temp=arr[0];
+        for(int i=1;  i<n; i++){
+            arr[i-1] = arr[i];
+        }
+
+        arr[n-1] = temp;
+    }
+    
+}
