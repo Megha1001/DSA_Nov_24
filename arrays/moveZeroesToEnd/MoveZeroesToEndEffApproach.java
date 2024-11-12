1dc91651fd3c8adba889dec02669c45a89043a73
Megha Verma <meghaverma1001@gmail.com>
2024-11-12 22:12:49 +0530

    Update MoveZeroesToEndEffApproach.java

--- arrays/moveZeroesToEnd/MoveZeroesToEndEffApproach.java
+++ arrays/moveZeroesToEnd/MoveZeroesToEndEffApproach.java
@@ -23,6 +23,23 @@ public class MoveZeroesToEndEffApproach {
 
     public static void moveZeroesToEndAndPrintArray(int arr[], int n){
 
+        int count = 0;
+
+        for(int i=0; i<n; i++){
+            if(arr[i]!=0){
+                //swap
+                int temp = arr[i];
+                arr[i]=arr[count];
+                arr[count]= temp;
+                ++count;
+            }
+        }
+
+        //print
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+
     }
 
 
