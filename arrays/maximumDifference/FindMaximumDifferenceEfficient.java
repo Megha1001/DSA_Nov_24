ab81ec7069a19151d4fe6beb48c147fb0cd13ecc
Megha Verma <meghaverma1001@gmail.com>
2024-11-13 22:26:24 +0530

    Update FindMaximumDifferenceEfficient.java

--- arrays/maximumDifference/FindMaximumDifferenceEfficient.java
+++ arrays/maximumDifference/FindMaximumDifferenceEfficient.java
@@ -2,4 +2,10 @@ package arrays.maximumDifference;
 
 public class FindMaximumDifferenceEfficient {
     
+
+    public static void main(String args[]){
+        int arr[]={2,3,10,6,4,8,1};
+        System.out.println("Maximum difference is "+findMaxDiff(arr, arr.length));
+    }
+    
 }
