c5fdddc62af94298eb1490e828614de980cdd99e
Megha Verma <meghaverma1001@gmail.com>
2024-11-13 23:05:07 +0530

    Create PrintFrequencyOfElementInSortedArrayEfficient2.java

---
+++ arrays/frequencyInSortedArray/PrintFrequencyOfElementInSortedArrayEfficient2.java
@@ -0,0 +1,36 @@
+package arrays.frequencyInSortedArray;
+
+/*
+ * TIME COMPLEXITY : Theta(N)
+ * AUXILIARY SPACE : Theta(1)
+ */
+
+public class PrintFrequencyOfElementInSortedArrayEfficient2 {
+
+    public static void main(String args[]){
+        int arr[] = {10,10,10,20,20,20,20,30,40,50};
+        System.out.println("Frequency of element in sorted array are : ");
+        findAndPrintFreq(arr, arr.length);
+    }
+    
+
+    public static void findAndPrintFreq(int arr[], int n){
+       
+        int freq=1, i=1;
+
+        while(i<n){
+
+            while(i<n && arr[i]==arr[i-1]){
+                ++freq;
+                ++i;
+            }
+            System.out.println(arr[i-1]+":"+freq);
+            freq=1;
+            ++i; // this is necessary as we have already considered freq=1 for next element.
+        }
+
+
+
+    }
+
+}
