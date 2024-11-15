77ea8b8dd89c06bf7ecd8c3c4e84923f18f21f09
Megha Verma <meghaverma1001@gmail.com>
2024-11-15 21:45:59 +0530

    Update StockBuyAndSellNaiveSolution.java

--- arrays/stocksBuyAndSell/StockBuyAndSellNaiveSolution.java
+++ arrays/stocksBuyAndSell/StockBuyAndSellNaiveSolution.java
@@ -6,21 +6,33 @@ public class StockBuyAndSellNaiveSolution {
     public static void main(String args[]){
         int arr[] = {1, 5, 3, 8, 12};
 
-        System.out.println("The maximum profit that we can have is : "+profit);
+        System.out.println("The maximum profit that we can have is : "+findMaxProfit(arr, 0 , arr.length-1));
     }  
 
 
     public static int findMaxProfit(int arr[], int start, int end){
 
-        if(start<=end){
+        if(start>=end){
             return 0;
         }
 
         for(int i=0; i<end;i++){ //here we are going till with i=n-2 since we can't find succeding pair for end element (n-1)
 
+            for(int j=i+1; j<=end; j++){
+
+                if(arr[j] > arr[i]){
+                    int currProfit = arr[j]-arr[i]
+                                    + findMaxProfit(arr, start, i-1)
+                                    + findMaxProfit(arr, j+1, end);
+                    
+                    profit = Math.max(profit, currProfit);
+                }
+
+            }
+
         }
 
-
+        return profit;
 
     }
 
