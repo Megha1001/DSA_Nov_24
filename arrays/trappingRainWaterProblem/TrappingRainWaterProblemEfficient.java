d1b03a0cc39d95d577d052abc76bab3acfe38223
Megha Verma <meghaverma1001@gmail.com>
2024-11-15 23:19:47 +0530

    Create TrappingRainWaterProblemEfficient.java

---
+++ arrays/trappingRainWaterProblem/TrappingRainWaterProblemEfficient.java
@@ -0,0 +1,8 @@
+package arrays.trappingRainWaterProblem;
+
+public class TrappingRainWaterProblemEfficient {
+    public static void main(String args[]){
+        int arr[] = {3, 0, 1, 2, 5};
+        System.out.println("Maximum water that can be stored with given input is "+getWater(arr, arr.length));
+    }
+}
