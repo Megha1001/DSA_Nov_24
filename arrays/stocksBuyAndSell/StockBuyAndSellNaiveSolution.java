421eb5705af51a14e4ab11f0255f1b85cfe17e54
Megha Verma <meghaverma1001@gmail.com>
2024-11-15 21:51:25 +0530

    Update StockBuyAndSellNaiveSolution.java

--- arrays/stocksBuyAndSell/StockBuyAndSellNaiveSolution.java
+++ arrays/stocksBuyAndSell/StockBuyAndSellNaiveSolution.java
@@ -1,7 +1,6 @@
 package arrays.stocksBuyAndSell;
 
 public class StockBuyAndSellNaiveSolution {
-    public static int profit=0;
 
     public static void main(String args[]){
         int arr[] = {1, 5, 3, 8, 12};
@@ -15,8 +14,9 @@ public class StockBuyAndSellNaiveSolution {
         if(start>=end){
             return 0;
         }
-
-        for(int i=0; i<end;i++){ //here we are going till with i=n-2 since we can't find succeding pair for end element (n-1)
+        //here we are going till with i=n-2 since we can't find succeding pair for end element (n-1)
+        int profit=0;
+        for(int i=start; i<end;i++){
 
             for(int j=i+1; j<=end; j++){
 
@@ -36,4 +36,5 @@ public class StockBuyAndSellNaiveSolution {
 
     }
 
+    
 }
