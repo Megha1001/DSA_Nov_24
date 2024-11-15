c1d899003547e6318bd4e10444da7578fc83991b
Megha Verma <meghaverma1001@gmail.com>
2024-11-15 23:04:01 +0530

    Update TrappingRainWaterProblemNaive.java

--- arrays/trappingRainWaterProblem/TrappingRainWaterProblemNaive.java
+++ arrays/trappingRainWaterProblem/TrappingRainWaterProblemNaive.java
@@ -3,7 +3,8 @@ package arrays.trappingRainWaterProblem;
 public class TrappingRainWaterProblemNaive {
 
     public static void main(String args[]){
-        
+        int arr[] = {3, 0, 1, 2, 5};
+        System.out.println("Maximum water that can be stored with given input is "+getWater(arr, arr.length));
     }
     
 }
