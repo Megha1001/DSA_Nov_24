799a8660d86711141d3fcebcf851c8435ab67248
Megha Verma <meghaverma1001@gmail.com>
2024-11-15 22:12:57 +0530

    Update StockBuyAndSellThroughValleyPeakApproach.java

--- arrays/stocksBuyAndSell/StockBuyAndSellThroughValleyPeakApproach.java
+++ arrays/stocksBuyAndSell/StockBuyAndSellThroughValleyPeakApproach.java
@@ -1,5 +1,11 @@
 package arrays.stocksBuyAndSell;
 
 public class StockBuyAndSellThroughValleyPeakApproach {
+
+    public static void main(String args[]){
+        int arr[] = {1,5,3,8,12};
+
+        System.out.println("The maximum profit that we can have is : "+findMaxProfit(arr, arr.length));
+    }
     
 }
