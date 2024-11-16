a08c8c13b8b8f4e8863da01144cecb49d095b980
Megha Verma <meghaverma1001@gmail.com>
2024-11-16 18:17:21 +0530

    Create FinaSubArrayWithMaximumSumUsingKadaneAlgo.java

---
+++ arrays/maximumSumSubarray/FinaSubArrayWithMaximumSumUsingKadaneAlgo.java
@@ -0,0 +1,13 @@
+package arrays.maximumSumSubarray;
+
+/*
+ * LOGIC : Use Kadence's algo
+ *  Find maximum sum of subarray that ends with this element.
+ *  max_ending(i) = Math.max(max_ending(i-1)+arr[i], arr[i])
+ */
+
+public class FinaSubArrayWithMaximumSumUsingKadaneAlgo {
+
+
+    
+}
