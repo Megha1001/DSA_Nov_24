b73b11c922d347427e8f829661e78b807918c564
Megha Verma <meghaverma1001@gmail.com>
2024-11-16 14:47:32 +0530

    Update CountConsecutiveOneEfficientSolution.java

--- arrays/consecutiveOnes/CountConsecutiveOneEfficientSolution.java
+++ arrays/consecutiveOnes/CountConsecutiveOneEfficientSolution.java
@@ -1,5 +1,10 @@
 package arrays.consecutiveOnes;
 
+/*
+ * TIME COMPLEXITY : Theta(N)
+ * AUXILIARY SPACE : Theta(1) 
+ */
+
 public class CountConsecutiveOneEfficientSolution {
 
     public static void main(String args[]){
