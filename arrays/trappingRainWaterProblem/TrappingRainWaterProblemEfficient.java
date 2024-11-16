4d718225b448861fd0c472c79da7182a0f62711b
Megha Verma <meghaverma1001@gmail.com>
2024-11-16 14:07:09 +0530

    Update TrappingRainWaterProblemEfficient.java

--- arrays/trappingRainWaterProblem/TrappingRainWaterProblemEfficient.java
+++ arrays/trappingRainWaterProblem/TrappingRainWaterProblemEfficient.java
@@ -8,7 +8,28 @@ public class TrappingRainWaterProblemEfficient {
 
 
     public static int getWater(int arr[], int n){
+
+        int res = 0;
         
+        int lMax [] = new int[n];
+        int rMax[] = new int[n];
+
+        lMax[0] = arr[0];
+        for(int i = 1; i < n; i++){
+            lMax[i] = Math.max(lMax[i-1], arr[i]);
+        }
+
+        rMax[n-1] = arr[n-1];
+        for(int i = n - 2; i >= 0; i--){
+            rMax[i] = Math.max(rMax[i+1], arr[i]);
+        }
+
+
+        for(int i=0; i<n; i++){
+            res+= Math.min(lMax[i], rMax[i]) - arr[i];
+        }
+
+        return res;
 
     }
 }
