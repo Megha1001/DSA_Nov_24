5c24f2e2fcad5eed4367f040620a856fd755410d
Megha Verma <meghaverma1001@gmail.com>
2024-11-16 14:33:15 +0530

    Create CountConsecutiveOneNaiveSolution.java

---
+++ arrays/consecutiveOnes/CountConsecutiveOneNaiveSolution.java
@@ -0,0 +1,34 @@
+package arrays.consecutiveOnes;
+
+public class CountConsecutiveOneNaiveSolution {
+
+    public static void main(String args[]){
+        int arr[] = {1, 0, 0, 0, 0};
+
+        System.out.println("Maximum consecutive 1's in given array is "+countConsecutiveOnes(arr, arr.length));
+    }
+
+    public static int countConsecutiveOnes(int arr[], int n){
+        int max = 0;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] == 1){
+                int count = 1;
+                for(int j = i + 1; j < n; j++){
+                    if(arr[j] == 1 && arr[j] == arr[j-1]){
+                        //consecutive
+                        ++count;
+                    }else{
+                        max = Math.max(max, count);
+                        count=1;
+                    }
+                    max = Math.max(max, count);
+                }
+                
+            }
+        }
+        
+        return max;
+    }
+    
+}
