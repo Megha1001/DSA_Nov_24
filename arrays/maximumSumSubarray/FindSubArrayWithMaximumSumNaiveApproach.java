3b69b8bea1966b46b83f0eed90b2af8b2a169742
Megha Verma <meghaverma1001@gmail.com>
2024-11-16 17:45:51 +0530

    Create FindSubArrayWithMaximumSumNaiveApproach.java

---
+++ arrays/maximumSumSubarray/FindSubArrayWithMaximumSumNaiveApproach.java
@@ -0,0 +1,13 @@
+package arrays.maximumSumSubarray;
+
+
+
+/*
+ * LOGIC : Find all the subarrays and update the res accordingly
+ * 1. Find all the subarray that can be constructed by numbers present at position 'i'
+ * 2. Update the res only when the curr sum > res
+ */
+
+public class FindSubArrayWithMaximumSumNaiveApproach {
+    
+}
