6554b147d7f9d45314ba32e98a8be25d7916b5ae
Megha Verma <meghaverma1001@gmail.com>
2024-11-16 20:18:34 +0530

    Update FingMaximumLengthEvenOddSubarrayNaiveSolution.java

--- arrays/maximumLengthEvenOddSubarray/FingMaximumLengthEvenOddSubarrayNaiveSolution.java
+++ arrays/maximumLengthEvenOddSubarray/FingMaximumLengthEvenOddSubarrayNaiveSolution.java
@@ -3,7 +3,9 @@ package arrays.maximumLengthEvenOddSubarray;
 public class FingMaximumLengthEvenOddSubarrayNaiveSolution {
 
     public static void main(String args[]){
-        int arr[] = {10, 12, 14, 7, 8};
+        // int arr[] = {10, 12, 14, 7, 8};
+        // int arr[] = {7, 10, 13, 14};
+        int arr[] = {10, 12, 14, 16};
 
         System.out.println("Maximum length of even-odd subarray is "+findMaxLengthEvenOddSubarray(arr, arr.length));
     }
