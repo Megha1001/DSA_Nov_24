5922a1c9b0fe09987bd5ffe359582ea009e990b6
Megha Verma <meghaverma1001@gmail.com>
2024-11-17 14:29:20 +0530

    Create FindMajorityElementNaiveSolution.java

---
+++ arrays/majorityElement/FindMajorityElementNaiveSolution.java
@@ -0,0 +1,11 @@
+package arrays.majorityElement;
+
+
+/*
+ * Majority element: An element is known as majority when it appears more than n/2 times
+ *  Logic  : Consider every element as majority
+ */
+
+public class FindMajorityElementNaiveSolution {
+    
+}
