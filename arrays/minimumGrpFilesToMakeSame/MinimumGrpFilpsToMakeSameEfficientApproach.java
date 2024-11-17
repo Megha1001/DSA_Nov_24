7272eb36ae40e8ff0f8b89dee894c69dc47528f8
Megha Verma <meghaverma1001@gmail.com>
2024-11-17 19:22:32 +0530

    Update MinimumGrpFilpsToMakeSameEfficientApproach.java

--- arrays/minimumGrpFilesToMakeSame/MinimumGrpFilpsToMakeSameEfficientApproach.java
+++ arrays/minimumGrpFilesToMakeSame/MinimumGrpFilpsToMakeSameEfficientApproach.java
@@ -13,11 +13,35 @@ package arrays.minimumGrpFilesToMakeSame;
 public class MinimumGrpFilpsToMakeSameEfficientApproach {
     
     public static void main(String args[]){
-        int arr[] = {0, 0, 1, 1, 0, 0, 1, 1, 0};
+        // int arr[] = {0, 0, 1, 1, 0, 0, 1, 1, 0};
+        int arr[] = {0, 0, 1, 1, 0, 0, 1, 1, 0, 1};
         System.out.println("Flips sequence is ");
         printFlips(arr, arr.length);
     }
 
+    public static void printFlips(int arr[], int n){
+
+        for(int i = 1; i < n; i++){
+
+            if(arr[i] != arr[i-1]) { //either the start of second group or end of second group
+
+                if(arr[i] != arr[0]){ //start
+                    System.out.print("From "+i+" to ");
+                }
+
+                else if (arr[i] != arr[i-1]){ //end of second group at i-1th position
+                    System.out.println(i-1); //NOTE : here we consider i-1
+                }
+
+            }
+
+        }
+
+        if(arr[n-1] != arr[0]){
+            System.out.println(n-1); //for case like :  0, 0, 1, 1, 0, 0, 1, 1, 0, 1. For the last element
+        }
+
+    }
 
 
 }
