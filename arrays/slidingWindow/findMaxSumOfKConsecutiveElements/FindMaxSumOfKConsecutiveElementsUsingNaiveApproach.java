82eee817b3e154d2a8b1dd79ad3ebe15cafc9736
Megha Verma <meghaverma1001@gmail.com>
2024-11-17 19:46:40 +0530

    Update FindMaxSumOfKConsecutiveElementsUsingNaiveApproach.java

---
+++ arrays/slidingWindow/findMaxSumOfKConsecutiveElements/FindMaxSumOfKConsecutiveElementsUsingNaiveApproach.java
@@ -0,0 +1,29 @@
+package arrays.slidingWindow.findMaxSumOfKConsecutiveElements;
+
+/*
+ * TIME COMPLEXITY : Theta(N^2)
+ * AUXILIARY SPACE : Theta(1)
+ */
+
+public class FindMaxSumOfKConsecutiveElementsUsingNaiveApproach {
+
+    public static void main(String args[]){
+        int arr[] = {1, 8, 30, -5, 20, 7};
+        int k = 3;
+        System.out.println("Maximum sum of k consecutive elements is "+findMaxSum(arr, arr.length, k));
+    }
+
+
+    public static int findMaxSum(int arr[], int n, int k){
+        int sum = 0;
+        for(int i = 0 ; i <= (n-k+1) ; i ++){
+            int currSum = 0;
+            for (int j = i; j < k; j++){
+                currSum += arr[j];
+            }
+            sum = Math.max(sum, currSum);
+        }
+        return sum;
+    }
+    
+}
