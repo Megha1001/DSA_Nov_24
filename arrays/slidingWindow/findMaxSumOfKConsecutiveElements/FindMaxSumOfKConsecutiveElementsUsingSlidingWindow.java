941705c9678fa1826db5cc5d3a96a48cf49cf9ad
Megha Verma <meghaverma1001@gmail.com>
2024-11-17 19:53:08 +0530

    Update FindMaxSumOfKConsecutiveElementsUsingSlidingWindow.java

--- arrays/slidingWindow/findMaxSumOfKConsecutiveElements/FindMaxSumOfKConsecutiveElementsUsingSlidingWindow.java
+++ arrays/slidingWindow/findMaxSumOfKConsecutiveElements/FindMaxSumOfKConsecutiveElementsUsingSlidingWindow.java
@@ -2,4 +2,27 @@ package arrays.slidingWindow.findMaxSumOfKConsecutiveElements;
 
 public class FindMaxSumOfKConsecutiveElementsUsingSlidingWindow {
     
+    public static void main(String args[]){
+        int arr[] = {1, 8, 30, -5, 20, 7};
+        int k = 3;
+        System.out.println("Maximum sum of k consecutive elements is "+findMaxSum(arr, arr.length, k));
+    }
+
+    public static int findMaxSum(int arr[], int n , int k){
+
+        //find sum of first k elements
+        int currSum = 0;
+        for(int i = 0; i < k; i++){
+            currSum += arr[i];
+        }
+        int res = currSum;
+        for(int i = 1; i<= (n - k - 1); i++){
+            currSum = currSum - arr[i-1] + arr[i+k-1];
+            res = Math.max(currSum, res);
+
+        }
+
+        return res;
+
+    }
 }
