b66e000b1ae2d024cdeef3f9aecf3c4eea1bd00a
Megha Verma <meghaverma1001@gmail.com>
2024-11-17 12:00:40 +0530

    Update FindMaximumCircularSubArrayNaive.java

--- arrays/maximumCircularSubarray/FindMaximumCircularSubArrayNaive.java
+++ arrays/maximumCircularSubarray/FindMaximumCircularSubArrayNaive.java
@@ -1,5 +1,10 @@
 package arrays.maximumCircularSubarray;
 
+/*
+ * LOGIC : Consider all subarray and find the maximum sum
+ * 1. Consider all the subarray starting with position i -> there can be N Subarray for every element in circular array case only (otherwise N-1)
+ */
+
 public class FindMaximumCircularSubArrayNaive {
 
     public static void main(String args[]){
@@ -8,7 +13,25 @@ public class FindMaximumCircularSubArrayNaive {
     }
 
     public static int findMaximumSumInCircularSubarray(int [] arr, int n){
-        
+
+        int res = arr[0];
+
+        for(int i = 0; i < n; i++){
+            int curr_sum = arr[i];
+            int curr_max = arr[i];
+
+            for(int j = 1; j < n; j++){ //should always run N times in case of circular array
+                int index = ( i + j ) % n;
+                curr_sum += arr[index];
+                curr_max = Math.max(curr_max, curr_sum);
+            }
+
+            res = Math.max(res, curr_max);
+
+        }
+
+        return res;
+
     }
     
 }
