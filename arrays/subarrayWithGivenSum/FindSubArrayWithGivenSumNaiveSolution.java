47fc12ae93abf46d25b08d346ed454109d419c0e
Megha Verma <meghaverma1001@gmail.com>
2024-11-18 22:58:58 +0530

    Update FindSubArrayWithGivenSumNaiveSolution.java

--- arrays/subarrayWithGivenSum/FindSubArrayWithGivenSumNaiveSolution.java
+++ arrays/subarrayWithGivenSum/FindSubArrayWithGivenSumNaiveSolution.java
@@ -7,5 +7,26 @@ public class FindSubArrayWithGivenSumNaiveSolution {
         int sum = 33;
         System.out.println("Do se have suarray with given sum : "+checkSum(arr, arr.length, sum));
     }
+
+    public static boolean checkSum(int arr[], int n, int x){
+        boolean res = false;
+
+        for(int i = 0; i < n; i++){
+            int curr = 0;
+            for(int j = i; j < n; j++){
+                curr += arr[j];
+                if(curr == x){
+                    return true;
+                }
+                else if(curr > x){
+                    break;
+                }
+            }
+        }
+
+
+        return res;
+
+    }
     
 }
