3f1a046281fa7e6ee17195715ff910fce2b59598
Megha Verma <meghaverma1001@gmail.com>
2024-11-19 21:31:17 +0530

    Update FindElementInSortedArrayUsingBinarySearchIterative.java

--- searching/binarySearch/FindElementInSortedArrayUsingBinarySearchIterative.java
+++ searching/binarySearch/FindElementInSortedArrayUsingBinarySearchIterative.java
@@ -3,8 +3,10 @@ package searching.binarySearch;
 public class FindElementInSortedArrayUsingBinarySearchIterative {
 
     public static void main(String args[]){
-        int arr[] = {1, 2, 3, 4, 5, 6};
-        int x = 2;
+        // int arr[] = {1, 2, 3, 4, 5, 6};
+        // int x = 2;
+        int arr[] = {5, 15, 25};
+        int x = 25;
 
         System.out.println("is the element present at what index in  the given array ? :"+isPresent(arr, arr.length, x));
     }
@@ -17,7 +19,6 @@ public class FindElementInSortedArrayUsingBinarySearchIterative {
         while(s <= e){
             int mid = s+ (e-s)/2; //to secure from overflow
 
-            System.out.println("mid : "+mid);
             if(arr[mid] == x){
                 return mid;
             }
