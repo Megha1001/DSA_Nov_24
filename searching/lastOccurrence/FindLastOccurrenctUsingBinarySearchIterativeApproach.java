9986047254f3f38dd0cee857f793130988c9f05a
Megha Verma <meghaverma1001@gmail.com>
2024-11-21 20:27:01 +0530

    Create FindLastOccurrenctUsingBinarySearchIterativeApproach.java

---
+++ searching/lastOccurrence/FindLastOccurrenctUsingBinarySearchIterativeApproach.java
@@ -0,0 +1,11 @@
+package searching.lastOccurrence;
+
+public class FindLastOccurrenctUsingBinarySearchIterativeApproach {
+
+    public static void main(String args[]){
+        int arr[] = {5, 10, 10, 15, 20, 20, 20};
+        int x = 20;
+        System.out.println("The last occurrence of "+x+" in given array is "+lastOccurrence(arr, 0, arr.length-1, x));
+    }
+    
+}
