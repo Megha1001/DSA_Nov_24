84d9f6a6c4b1b04669f7c17de247b2c122f9e827
Megha Verma <meghaverma1001@gmail.com>
2024-11-21 19:40:09 +0530

    Update FindLastOccurrenceUsingBinarySearchRecursiveSolution.java

--- searching/lastOccurrence/FindLastOccurrenceUsingBinarySearchRecursiveSolution.java
+++ searching/lastOccurrence/FindLastOccurrenceUsingBinarySearchRecursiveSolution.java
@@ -2,4 +2,13 @@ package searching.lastOccurrence;
 
 public class FindLastOccurrenceUsingBinarySearchRecursiveSolution {
     
+    public static void main(String args[]){
+        int arr[] = {5, 10, 10, 15, 20, 20, 20};
+        int x = 20;
+        System.out.println("The last occurrence of "+x+" in given array is "+lastOccurrence(arr, 0, arr.length-1, x));
+    }
+
+    public static int lastOccurrence(int arr[], int low, int high , )
+
+
 }
