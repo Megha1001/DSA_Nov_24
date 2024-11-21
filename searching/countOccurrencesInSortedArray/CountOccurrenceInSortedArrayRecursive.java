a71246937b5ce2dbd1b3183f6893edba177c0cba
Megha Verma <meghaverma1001@gmail.com>
2024-11-21 21:17:45 +0530

    Update CountOccurrenceInSortedArrayRecursive.java

--- searching/countOccurrencesInSortedArray/CountOccurrenceInSortedArrayRecursive.java
+++ searching/countOccurrencesInSortedArray/CountOccurrenceInSortedArrayRecursive.java
@@ -11,6 +11,20 @@ public class CountOccurrenceInSortedArrayRecursive {
 
     public static int countOccurrence(int arr[], int low, int high, int x){
         
+        if(low > high){
+            return 0;
+        }
+
+        int mid = low + (high-low)/2;
+        if(arr[mid]==x){
+            // 1 + count occurence in left  + count occurence in right
+            return 1 + countOccurrence(arr, low, mid-1, x) + countOccurrence(arr, mid+1 , high, x);
+        }
+        else if(arr[mid] > x){
+            return countOccurrence(arr, low, mid-1, x);
+        }else{
+            return countOccurrence(arr, mid+1, high, x);
+        }
     }
     
 }
