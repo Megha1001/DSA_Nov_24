8437953291a24b1a5439a01a985f6da556bafcee
Megha Verma <meghaverma1001@gmail.com>
2024-11-21 19:28:24 +0530

    Update FindFirstOccurrenceUsingBinarySearchIterativeApproach.java

---
+++ searching/firstOccurrence/FindFirstOccurrenceUsingBinarySearchIterativeApproach.java
@@ -0,0 +1,38 @@
+package searching.firstOccurrence;
+
+public class FindFirstOccurrenceUsingBinarySearchIterativeApproach {
+
+    public static void main(String args[]){
+        int arr[] = {5, 10, 10, 15, 20, 20, 20};
+        int x = 20;
+        System.out.println("The first occurrence of "+x+" in given array is "+findFirstOccurrence(arr, 0, arr.length, x));
+    }
+
+
+    public static int findFirstOccurrence(int arr[], int low , int high, int x){
+
+        while(low <= high){
+            int mid = low  + (high-low)/2;
+
+            if(arr[mid] > x){
+                //go left 
+                high = mid-1;
+            }
+            else if(arr[mid] < x){
+                //go to right
+                low = mid+1;
+            }
+            else if( mid==0 || arr[mid] != arr[mid-1]){
+                return mid;
+            }else{
+                high = mid-1;
+            }
+
+        }
+
+        return -1;
+
+
+    }
+    
+}
