a047185aab8ab6da7e80549e87ff160b6947373a
Megha Verma <meghaverma1001@gmail.com>
2024-11-21 20:58:48 +0530

    Create CountOccurrenceInSortedArrayRecursive.java

---
+++ searching/countOccurrencesInSortedArray/CountOccurrenceInSortedArrayRecursive.java
@@ -0,0 +1,16 @@
+package searching.countOccurrencesInSortedArray;
+
+public class CountOccurrenceInSortedArrayRecursive {
+    
+    public static void main(String args[]){
+        int arr[] = {10,20,20,20,30,50};
+        int x = 20;
+        System.out.println("The occurrence of "+x+ " is "+countOccurrence(arr, 0, arr.length-1, x));
+    }
+
+
+    public static int countOccurrence(int arr[], int low, int high, int x){
+        
+    }
+    
+}
