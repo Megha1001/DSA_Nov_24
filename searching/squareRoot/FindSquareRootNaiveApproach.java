284d97b995595df903c63022ae5e5b8323d4575b
Megha Verma <meghaverma1001@gmail.com>
2024-11-23 00:01:29 +0530

    Update FindSquareRootNaiveApproach.java

---
+++ searching/squareRoot/FindSquareRootNaiveApproach.java
@@ -0,0 +1,19 @@
+package searching.squareRoot;
+
+public class FindSquareRootNaiveApproach {
+    
+    public static void main(String args[]){
+        int num = 19;
+        System.out.println("Square root of given number is : "+findSqr(num));
+    }
+
+    public static int findSqr(int num){
+        //corner-case
+    
+        int i = 1;
+        while()
+
+        return i;
+    }
+    
+}
