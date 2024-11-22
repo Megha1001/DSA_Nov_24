a8e5934e349020c2f6c25738371926af0bbafb50
Megha Verma <meghaverma1001@gmail.com>
2024-11-22 21:20:13 +0530

    Create CountOnesInSortedArrayUsingBinarySearch.java

---
+++ searching/countOnesInSortedArray/CountOnesInSortedArrayUsingBinarySearch.java
@@ -0,0 +1,10 @@
+package searching.countOnesInSortedArray;
+
+public class CountOnesInSortedArrayUsingBinarySearch {
+
+    public static void main(String args[]){
+        int arr[] = {0,1,1,1,1,1};
+        System.out.println("The number of 1's in given array is  : "+countOnes(arr, 0, arr.length-1));
+    }
+    
+}
