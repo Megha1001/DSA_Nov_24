50673912f1e97d7982ea3a80460a6f7e7ccf3918
Megha Verma <meghaverma1001@gmail.com>
2024-11-22 21:22:58 +0530

    Update CountOnesInSortedArrayUsingBinarySearch.java

--- searching/countOnesInSortedArray/CountOnesInSortedArrayUsingBinarySearch.java
+++ searching/countOnesInSortedArray/CountOnesInSortedArrayUsingBinarySearch.java
@@ -6,5 +6,22 @@ public class CountOnesInSortedArrayUsingBinarySearch {
         int arr[] = {0,1,1,1,1,1};
         System.out.println("The number of 1's in given array is  : "+countOnes(arr, 0, arr.length-1));
     }
+
+
+    public static int countOnes(int arr[], int low, int high){
+
+        if(low > high){
+            return 0;
+        }
+
+        int mid = low + (high-low)/2;
+
+        if(arr[mid]==1){
+            return 1+ (high-low) + countOnes(arr, low, mid-1); //1 + 1's on right side + count on left side
+        }else{ // arr[mid]==0 //search on right side
+            return  countOnes(arr, mid+1, high);
+        }
+
+    }
     
 }
