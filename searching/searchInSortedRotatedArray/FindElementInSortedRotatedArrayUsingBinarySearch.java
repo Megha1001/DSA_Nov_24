f0f70f981c985fcad8f8a149216b675ef67108d2
Megha Verma <meghaverma1001@gmail.com>
2024-11-23 16:18:17 +0530

    Update FindElementInSortedRotatedArrayUsingBinarySearch.java

--- searching/searchInSortedRotatedArray/FindElementInSortedRotatedArrayUsingBinarySearch.java
+++ searching/searchInSortedRotatedArray/FindElementInSortedRotatedArrayUsingBinarySearch.java
@@ -7,5 +7,37 @@ public class FindElementInSortedRotatedArrayUsingBinarySearch {
         int x = 500;
         System.out.println("Element is present in given array at location : "+searchInSortedRotatedArray(arr, arr.length, x));
     }
+
+    public static int searchInSortedRotatedArray(int arr[], int n, int x){
+        int low = 0, high = n-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if(arr[mid] == x){
+                return mid;
+            }
+
+            //if left half is sorted and element is present there
+            if(arr[low] <= arr[mid]){
+                if(x >= arr[low] && x < arr[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }
+            //if righ half is sorted and element is there
+            else {
+                if(x > arr[mid] && x <= arr[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+
+        }
+        return -1;
+
+
+    }
     
 }
