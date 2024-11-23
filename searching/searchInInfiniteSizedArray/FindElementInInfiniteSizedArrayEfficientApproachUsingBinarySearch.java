02c5778bb3cab0032a66a94d78d91ef28b06f299
Megha Verma <meghaverma1001@gmail.com>
2024-11-23 13:37:54 +0530

    Update FindElementInInfiniteSizedArrayEfficientApproachUsingBinarySearch.java

--- searching/searchInInfiniteSizedArray/FindElementInInfiniteSizedArrayEfficientApproachUsingBinarySearch.java
+++ searching/searchInInfiniteSizedArray/FindElementInInfiniteSizedArrayEfficientApproachUsingBinarySearch.java
@@ -16,6 +16,38 @@ public class FindElementInInfiniteSizedArrayEfficientApproachUsingBinarySearch {
 
     public static int findElement(int arr[], int x){
         int i = 1;
+        while(arr[i] < x){
+            i = i*2;
+        }//after this there can only be two possibilities , either arr[i] == x, or arr[i] > x
+        /*
+         * if arr[i]==x , we found the element
+         * else --> we found the ending idx until we have to search in the given infinite sized array
+         */
+
+        if(arr[i] == x){
+            return i;
+        }else{
+            return binarySearch(arr, i/2+1, i-1, x);
+        }
+    }
+
+    public static int binarySearch(int arr[], int low , int high, int x){
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(arr[mid]==x){
+                return mid;
+            }
+            else if(arr[mid] > x){
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+        }
+
+        return -1;
+
     }
 
 }
