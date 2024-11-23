066e69b60e95ef330d8bfe8809f25014ebc92fc9
Megha Verma <meghaverma1001@gmail.com>
2024-11-23 13:34:44 +0530

    Create FindElementInInfiniteSizedArrayEfficientApproachUsingBinarySearch.java

---
+++ searching/searchInInfiniteSizedArray/FindElementInInfiniteSizedArrayEfficientApproachUsingBinarySearch.java
@@ -0,0 +1,21 @@
+package searching.searchInInfiniteSizedArray;
+
+/*
+ * TIME COMPLEXITY : O(log(positition))
+ * AUXILIARY SPACE : Theta(1)
+ */
+
+public class FindElementInInfiniteSizedArrayEfficientApproachUsingBinarySearch {
+
+    public static void main(String args[]){
+        int arr[] = {1,3,5,6,7,9,10,50,64,90};
+        int x = 64;
+        System.out.println("The element is present at : "+findElement(arr,x));
+    }
+    
+
+    public static int findElement(int arr[], int x){
+        int i = 1;
+    }
+
+}
