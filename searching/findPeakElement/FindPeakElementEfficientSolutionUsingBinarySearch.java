b701d07b001d5a02fa90b525ddb90e4dd9188d18
Megha Verma <meghaverma1001@gmail.com>
2024-11-24 12:38:08 +0530

    Create FindPeakElementEfficientSolutionUsingBinarySearch.java

---
+++ searching/findPeakElement/FindPeakElementEfficientSolutionUsingBinarySearch.java
@@ -0,0 +1,12 @@
+package searching.findPeakElement;
+
+/*
+ * Peak Element : Its 
+ */
+
+public class FindPeakElementEfficientSolutionUsingBinarySearch {
+    public static void main(String args[]){
+        int arr[] = {6, 7, 8, 20, 12};
+        System.out.println("The peak element in given array is : "+findPeakElement(arr, arr.length-1));
+    }
+}
