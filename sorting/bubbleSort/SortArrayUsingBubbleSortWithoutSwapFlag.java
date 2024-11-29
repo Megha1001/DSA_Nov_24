9698b0b9993dba77467c19311a92bba45ade7f38
Megha Verma <meghaverma1001@gmail.com>
2024-11-29 20:50:17 +0530

    Update SortArrayUsingBubbleSortWithoutSwapFlag.java

--- sorting/bubbleSort/SortArrayUsingBubbleSortWithoutSwapFlag.java
+++ sorting/bubbleSort/SortArrayUsingBubbleSortWithoutSwapFlag.java
@@ -1,4 +1,13 @@
 package sorting.bubbleSort;
+/*
+ * Logic :
+ * 1. Compare adjacent element.
+ *  -> if arr[i] >arr[i+1] -->swap the element
+ * 2. After every pass the greatest element in the avaiable array will go to its correct postition
+ * 
+ * Max Number of Pass required = N-1
+ * Max Number of comparision required = N(N-1)/2
+ */
 
 public class SortArrayUsingBubbleSortWithoutSwapFlag {
 
@@ -7,6 +16,14 @@ public class SortArrayUsingBubbleSortWithoutSwapFlag {
 
         System.out.println("Array after sorting is : ");
         sortUsingBubbleSort(arr, arr.length);
+    }
+
+    public static void sortUsingBubbleSort(int arr[], int n){
+        
+        for(int i=0; i<n-1; i++){
+
+        }
+
     }
     
 }
