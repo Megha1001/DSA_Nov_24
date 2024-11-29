0d311709b6da67e428c32e571236d0d9c3abaf12
Megha Verma <meghaverma1001@gmail.com>
2024-11-29 20:47:57 +0530

    Update SortArrayUsingBubbleSortWithoutSwapFlag.java

--- sorting/bubbleSort/SortArrayUsingBubbleSortWithoutSwapFlag.java
+++ sorting/bubbleSort/SortArrayUsingBubbleSortWithoutSwapFlag.java
@@ -4,6 +4,9 @@ public class SortArrayUsingBubbleSortWithoutSwapFlag {
 
     public static void main(String args[]){
         int arr[] = {2,10,8,7};
+
+        System.out.println("Array after sorting is : ");
+        sortUsingBubbleSort(arr, arr.length);
     }
     
 }
