89944fa3c6b6a5385d8cca1cddef592521dbbcb8
Megha Verma <meghaverma1001@gmail.com>
2024-12-02 21:55:43 +0530

    Update SortArrayUsingInsertionSort.java

--- sorting/insertionSort/SortArrayUsingInsertionSort.java
+++ sorting/insertionSort/SortArrayUsingInsertionSort.java
@@ -1,6 +1,17 @@
 package sorting.insertionSort;
 
-
+/*
+ * LOGIC : 
+ * - We'll having two sections of array -> sorted and one that needs sorting
+ * - For current element will place it at correct position in sorted array
+ * - Increase sorted array length by 1 after every iteration
+ * 
+ * In place and stable
+ * Used in practice for small arrays -> TimSort , IntroSort
+ * 
+ * TIME COMPLEXITY : O(N^2)
+ * Best case : O(N)
+ */
 
 public class SortArrayUsingInsertionSort {
     
