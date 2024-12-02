0ea5f21aca3631450bcd6c409f8f7f52bd901e93
Megha Verma <meghaverma1001@gmail.com>
2024-12-02 20:17:17 +0530

    Create SortArrayUsingSelectionSort.java

---
+++ sorting/selectionSort/SortArrayUsingSelectionSort.java
@@ -0,0 +1,39 @@
+package sorting.selectionSort;
+
+public class SortArrayUsingSelectionSort {
+
+    public static void main(String args[]){
+        int arr[] = {10, 5, 8, 20, 2, 18};
+        System.out.println("Array after sorting using selection sort is : ");
+        sortUsingSelectionSortTechnique(arr, arr.length);
+
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+
+    }
+
+    public static void sortUsingSelectionSortTechnique(int arr[], int n){
+        
+        //for every iteration, we find minimum element and swap with the arr[i]
+
+        for(int i = 0; i < n-1; i++){ //last element will automatically be at its original position
+            int minIdx = i;
+            for(int j = i+1; j < n; j++){
+                if(arr[minIdx] > arr[j]){
+                    minIdx = j;
+                }
+            }
+
+            //swap arr[j] with arr[midIdx]
+            if(i != minIdx){
+                int temp = arr[i];
+                arr[i] = arr[minIdx];
+                arr[minIdx] = temp;
+            }
+        }
+
+    }
+    
+}
