887c3443bda224d4b1ae9c029d38c3db93b1df5b
Megha Verma <meghaverma1001@gmail.com>
2024-12-02 22:16:45 +0530

    Update SortArrayUsingInsertionSort.java

--- sorting/insertionSort/SortArrayUsingInsertionSort.java
+++ sorting/insertionSort/SortArrayUsingInsertionSort.java
@@ -22,9 +22,28 @@ public class SortArrayUsingInsertionSort {
         sortUsingInsertionSort(arr, arr.length);
         
         for(int i = 0; i < arr.length; i++){
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
         
+    }
+
+
+    public static void sortUsingInsertionSort(int arr[], int n){
+
+        for(int i = 1; i < n; i++){
+
+            int key = arr[i];
+            int j = i-1;
+            
+            while(j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            arr[j+1] = key;
+
+        }
+
     }
     
 }
