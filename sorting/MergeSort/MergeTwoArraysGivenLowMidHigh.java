ec72fdf8b1e56ba574e5f6c9a801ac2217a9e104
Megha Verma <meghaverma1001@gmail.com>
2024-12-03 20:10:04 +0530

    Update MergeTwoArraysGivenLowMidHigh.java

--- sorting/MergeSort/MergeTwoArraysGivenLowMidHigh.java
+++ sorting/MergeSort/MergeTwoArraysGivenLowMidHigh.java
@@ -2,6 +2,16 @@ package sorting.MergeSort;
 
 public class MergeTwoArraysGivenLowMidHigh {
 
-    
+    public static void main(String args[]){
+        int arr [] = {10,20,40,20,30};
+        int low = 0, mid = 2, high = arr.length-1;
+        
+        System.out.println("After merge function the arrays is : ");
+        merge(arr, low, mid, high);
+
+        for(int i = 0; i < high; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
     
 }
