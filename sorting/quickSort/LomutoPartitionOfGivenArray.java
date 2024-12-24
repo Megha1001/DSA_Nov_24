05b8820427bdc2ca116bc81e9dc1cb946f959e58
Megha Verma <meghaverma1001@gmail.com>
2024-12-24 13:58:31 +0530

    Update LomutoPartitionOfGivenArray.java

--- sorting/quickSort/LomutoPartitionOfGivenArray.java
+++ sorting/quickSort/LomutoPartitionOfGivenArray.java
@@ -12,5 +12,29 @@ public class LomutoPartitionOfGivenArray {
         }
     }
 
+
+    public static int partitionArrayAroundPivot(int arr[], int low, int high, int pivot){
+
+        int i = low-1;
+
+        for(int j=low; j<=high; j++){
+            if(arr[j] < arr[pivot]){
+                ++i;
+                //swap i and j
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        //swap i+1 and pivot
+        int temp = arr[pivot];
+        arr[pivot] = arr[i+1];
+        arr[i+1] = temp;
+
+        return i+1 ; //index of pivot
+
+    }
+
     
 }
