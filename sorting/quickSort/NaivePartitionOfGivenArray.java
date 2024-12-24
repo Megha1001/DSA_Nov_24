216c752c70394336aa3b5cef2741d722100f68ca
Megha Verma <meghaverma1001@gmail.com>
2024-12-24 13:22:29 +0530

    Update NaivePartitionOfGivenArray.java

--- sorting/quickSort/NaivePartitionOfGivenArray.java
+++ sorting/quickSort/NaivePartitionOfGivenArray.java
@@ -1,51 +1,41 @@
 package sorting.quickSort;
 
-public class PartitionOfGivenArray {
+public class NaivePartitionOfGivenArray {
 
     public static void main(String args[]){
         int arr[] = {3, 8, 6, 12, 10, 7};
 
-        partitionArrayAroundPivot(arr, arr.length, 5);
+        partitionArrayAroundPivot(arr, 0 ,arr.length-1, 5);
         System.out.println("Array after partitioning is : ");
         for(int i=0; i < arr.length; i++){
             System.out.print(arr[i] +" ");
         }
     }
 
-    public static void partitionArrayAroundPivot(int arr[], int n, int pivot){
+    public static void partitionArrayAroundPivot(int arr[], int l, int h, int pivot){
 
-        int low = 0;
-        int high = pivot-1;
+        int temp[] = new int[h-l+1];
+        int index = 0;
 
-        while(low < high){
-
-            if(arr[low] <= arr[pivot]){
-                ++low;
+        for(int i=l; i<=h; i++){
+            if(arr[i] <= arr[pivot] && i!=pivot){ //inplace
+                temp[index++] = arr[i];
             }
-
-            System.out.println("1low : "+low+ " |high : "+high);
-
-            if(arr[high] > arr[pivot]){
-                --high;
-            }
-            System.out.println("2low : "+low+ " |high : "+high);
-            if(arr[high]< arr[pivot] && arr[low] > arr[pivot]){
-                //swap high and low
-                int temp = arr[low];
-                arr[low] = arr[high];
-                arr[high] = temp;
-
-                ++low; --high;
-            }
-
-            System.out.println("3low : "+low+ " |high : "+high);
-
         }
-        
-        //swap low+1 with pivot
-        int temp = arr[low+1];
-        arr[low+1] = arr[pivot];
-        arr[pivot] = temp;
+
+        temp[index++] = arr[pivot];
+
+        for(int i=l; i<=h; i++){
+            if(arr[i] > arr[pivot]){
+                temp[index++] = arr[i];
+            }
+        }
+
+
+        for(int i = l; i <= h; i++){
+            arr[i] = temp[i-l]; // temp index starts from 0
+        }
+
 
     }
     
