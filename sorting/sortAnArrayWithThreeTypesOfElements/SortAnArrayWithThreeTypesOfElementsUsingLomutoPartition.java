f777d2a8a86d199546d524f1168bf1d3be3d0be5
Megha Verma <meghaverma1001@gmail.com>
2024-12-27 13:48:17 +0530

    Update SortAnArrayWithThreeTypesOfElementsUsingLomutoPartition.java

--- sorting/sortAnArrayWithThreeTypesOfElements/SortAnArrayWithThreeTypesOfElementsUsingLomutoPartition.java
+++ sorting/sortAnArrayWithThreeTypesOfElements/SortAnArrayWithThreeTypesOfElementsUsingLomutoPartition.java
@@ -6,10 +6,40 @@ public class SortAnArrayWithThreeTypesOfElementsUsingLomutoPartition {
         int len = arr.length;
 
         System.out.println("Elements after partitioning is : ");
-        segregateElements(arr, len);
+        segregateElements(arr, 0 , len-1);
 
         for(int i=0; i<len; i++){
             System.out.print(arr[i]+ " ");
         }
+    }
+
+    public static void segregateElements(int arr[], int low, int high){
+
+        int pivot = segregateTwoElements(arr, low, high, 0);
+        segregateTwoElements(arr, pivot, high, 1);
+
+    }
+
+    public static int segregateTwoElements(int arr[], int low, int high, int elementToConsider){
+
+        int i = low-1;
+        int pivot = high;
+
+        for(int j = low; j < high; j++){
+            if(arr[j]== elementToConsider){
+                ++i;
+                //swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        //swap arr[i+1] and arr[pivot]
+        int temp = arr[i+1];
+        arr[i+1] = arr[pivot];
+        arr[pivot] = temp;
+
+        return i+1;
     }
 }
