c856bae0741f2fea37152b4d723aa8a2d33d5df6
Megha Verma <meghaverma1001@gmail.com>
2024-12-27 11:39:47 +0530

    Update ChocolateDistributionProblem.java

--- sorting/chocolateDistributionProblem/ChocolateDistributionProblem.java
+++ sorting/chocolateDistributionProblem/ChocolateDistributionProblem.java
@@ -10,6 +10,9 @@ import java.util.Arrays;
  * LOGIC : Sort the array
  * 1. after sorting consider each element as begining of minimum difference subset and find all such subset difference 
  * 2. then pick minimum out of them
+ * 
+ * TIME COMPLEXITY : O(nLogN)
+ * AUXILIARY SPACE : Theta(1)
  */
 
 public class ChocolateDistributionProblem {
@@ -28,11 +31,11 @@ public class ChocolateDistributionProblem {
             return -1;
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(arr); //O(nLogN)
 
         int res = arr[m-1] - arr[0];
 
-        for(int i=1; (i+m-1) < n; i++){
+        for(int i=1; (i+m-1) < n; i++){ //Theta(N)
             res = Math.min(res, arr[i+m-1]- arr[i]);
         }
 
