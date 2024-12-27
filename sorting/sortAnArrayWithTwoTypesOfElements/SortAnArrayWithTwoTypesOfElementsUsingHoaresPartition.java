ca0b1fb2a8dc9c9832e4a952cfe8159c996b09f0
Megha Verma <meghaverma1001@gmail.com>
2024-12-27 13:15:32 +0530

    Update SortAnArrayWithTwoTypesOfElementsUsingHoaresPartition.java

--- sorting/sortAnArrayWithTwoTypesOfElements/SortAnArrayWithTwoTypesOfElementsUsingHoaresPartition.java
+++ sorting/sortAnArrayWithTwoTypesOfElements/SortAnArrayWithTwoTypesOfElementsUsingHoaresPartition.java
@@ -1,5 +1,12 @@
 package sorting.sortAnArrayWithTwoTypesOfElements;
 
+/*
+ * TIME COMPLEXITY : O(N)
+ * AUXILIARY SPACE : Theta(1)
+ * 
+ * LOGIC : This is the variation of partitioning algo of quick Sort. This can be solved by using Naive, Lomuto & Hoare's partition. Here, we're using Lomuto Partition.
+ */
+
 public class SortAnArrayWithTwoTypesOfElementsUsingHoaresPartition {
     public static void main(String args[]){
         int arr[] = {13, -12, 18, -10, 10, -19};
@@ -9,5 +16,10 @@ public class SortAnArrayWithTwoTypesOfElementsUsingHoaresPartition {
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
+    }
+
+    public static void segregatePosAndNeg(int arr[], int low, int high){
+        int i = low-1;
+
     }
 }
