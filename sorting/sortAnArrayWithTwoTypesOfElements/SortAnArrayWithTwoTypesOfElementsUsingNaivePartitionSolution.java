f978eed130f7fad98b59e1e058f23eb80c1f8c09
Megha Verma <meghaverma1001@gmail.com>
2024-12-27 12:55:46 +0530

    Update SortAnArrayWithTwoTypesOfElementsUsingNaivePartitionSolution.java

--- sorting/sortAnArrayWithTwoTypesOfElements/SortAnArrayWithTwoTypesOfElementsUsingNaivePartitionSolution.java
+++ sorting/sortAnArrayWithTwoTypesOfElements/SortAnArrayWithTwoTypesOfElementsUsingNaivePartitionSolution.java
@@ -5,9 +5,46 @@ package sorting.sortAnArrayWithTwoTypesOfElements;
  * 1. Segregate negative and positive elements
  * 2. segregate even and odd elements
  * 3. Sort a binary array(containing 0's and 1's)
+ * 
+ * Logic : Use Naive approach of quick Sort partition.
  */
 
 
 public class SortAnArrayWithTwoTypesOfElementsUsingNaivePartitionSolution {
+
+    public static void main(String args[]){
+        int arr[] = {13, -12, 18, -10};
+        System.out.println("Array after partition is : ");
+        segregatePosAndNeg(arr, arr.length);
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+    public static void segregatePosAndNeg(int arr[], int n){
+
+        int temp[] = new int[n];
+        int idx = 0;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] < 0){
+                temp[idx++] = arr[i];
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            if(arr[i] >=0){
+                temp[idx++] = arr[i];
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            arr[i] = temp[i];
+        }
+
+
+    }
     
 }
