3606097330315280ae52f3dd7d610fe230a11e7a
Megha Verma <meghaverma1001@gmail.com>
2024-12-27 11:53:55 +0530

    Create SortAnArrayWithTwoTypesOfElementsUsingNaivePartitionSolution.java

---
+++ sorting/sortAnArrayWithTwoTypesOfElements/SortAnArrayWithTwoTypesOfElementsUsingNaivePartitionSolution.java
@@ -0,0 +1,13 @@
+package sorting.sortAnArrayWithTwoTypesOfElements;
+
+/*
+ * Segregate elements that has two types of elements
+ * 1. Segregate negative and positive elements
+ * 2. segregate even and odd elements
+ * 3. Sort a binary array(containing 0's and 1's)
+ */
+
+
+public class SortAnArrayWithTwoTypesOfElementsUsingNaivePartitionSolution {
+    
+}
