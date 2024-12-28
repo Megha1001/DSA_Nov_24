d3266b7897476e87bee6191a42ff2510c6abb8d9
Megha Verma <meghaverma1001@gmail.com>
2024-12-28 13:08:15 +0530

    Update PrintMatrixInSpiralPattern.java

--- matrix/spiralPattern/PrintMatrixInSpiralPattern.java
+++ matrix/spiralPattern/PrintMatrixInSpiralPattern.java
@@ -8,5 +8,18 @@ public class PrintMatrixInSpiralPattern {
         System.out.println("Elements of matrix after printing in spiral form are : ");
         printSpiralForm(arr);
     }
+
+    public static void printSpiralForm(int arr[][]){
+        int r = arr.length;
+        int c = arr[0].length;
+
+        int top = 0;
+        int right = c-1;
+        int bottom = r-1;
+        int left = 0;
+
+
+        
+    }
     
 }
