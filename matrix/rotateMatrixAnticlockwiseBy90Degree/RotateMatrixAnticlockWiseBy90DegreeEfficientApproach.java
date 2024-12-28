23f262929b926beac10f4bc4cf36e06ff3cb55b5
Megha Verma <meghaverma1001@gmail.com>
2024-12-28 12:32:53 +0530

    Update RotateMatrixAnticlockWiseBy90DegreeEfficientApproach.java

--- matrix/rotateMatrixAnticlockwiseBy90Degree/RotateMatrixAnticlockWiseBy90DegreeEfficientApproach.java
+++ matrix/rotateMatrixAnticlockwiseBy90Degree/RotateMatrixAnticlockWiseBy90DegreeEfficientApproach.java
@@ -14,7 +14,7 @@ public class RotateMatrixAnticlockWiseBy90DegreeEfficientApproach {
 
         System.out.println("Matrix elements after rotating by 90 degree are : ");
         rotateBy90DegreeAnticlockwise(arr);
-
+        System.out.println("----------------");
 
         for(int i=0; i<arr.length; i++){
             for(int j=0; j< arr[i].length; j++){
@@ -26,6 +26,33 @@ public class RotateMatrixAnticlockWiseBy90DegreeEfficientApproach {
 
 
     public static void rotateBy90DegreeAnticlockwise(int arr[][]){
-        
+        int n = arr.length; //Assuming square matrix
+        findTranspose(arr);
+
+        //reverse individual columns
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                //swap
+                System.out.println(arr[j][i] + "<->"+arr[n-1-i][i]);
+                System.out.println(arr[j][i] + "<->"+arr[n-1-i][i]);
+                int temp = arr[j][i];
+                arr[j][i] = arr[n-1-i][i];
+                arr[n-1-i][i] = temp;
+            }
+        }
+
     }
+
+    public static void findTranspose(int arr[][]){
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                //swap
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+    }
+
 }
