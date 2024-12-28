ca8fccce7186cba5f3ec1a54a60bc1788946ed2a
Megha Verma <meghaverma1001@gmail.com>
2024-12-28 13:27:45 +0530

    Update PrintMatrixInSpiralPattern.java

--- matrix/spiralPattern/PrintMatrixInSpiralPattern.java
+++ matrix/spiralPattern/PrintMatrixInSpiralPattern.java
@@ -1,14 +1,22 @@
 package matrix.spiralPattern;
 
 /*
- * 
+ * TIME COMPLEXITY : Theta(R*C)
+ * AUXILIARY SPACE : O(1)
  */
 
 public class PrintMatrixInSpiralPattern {
 
     public static void main(String args[]){
         // int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        // int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        int arr[][] = {// -> for this kind of input we need to have nested condition to check top<=bottom and left<=right inside while loop
+            {1},
+            {2},
+            {3},
+            {4},
+            {5}
+        };
 
         System.out.println("Elements of matrix after printing in spiral form are : ");
         printSpiralForm(arr);
@@ -37,17 +45,23 @@ public class PrintMatrixInSpiralPattern {
             }
             --right;
 
-            //print bottom
-            for(int i=right; i>=left; i--){
-                System.out.print(arr[bottom][i]+ " ");
+            if(top <= bottom){
+                //print bottom
+                for(int i=right; i>=left; i--){
+                    System.out.print(arr[bottom][i]+ " ");
+                }
+                --bottom;
             }
-            --bottom;
+            
 
-            //print left
-            for(int i=bottom; i>=top; i--){
-                System.out.print(arr[i][left]+ " ");
+            if(left <= right){
+                //print left
+                for(int i=bottom; i>=top; i--){
+                    System.out.print(arr[i][left]+ " ");
+                }
+                ++left;
             }
-            ++left;
+            
 
         }
 
