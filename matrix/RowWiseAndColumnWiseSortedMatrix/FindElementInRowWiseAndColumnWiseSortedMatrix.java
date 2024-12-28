45dc6b99d45c48fadf210f17baee00fbf7efffa5
Megha Verma <meghaverma1001@gmail.com>
2024-12-28 18:11:12 +0530

    Create FindElementInRowWiseAndColumnWiseSortedMatrix.java

---
+++ matrix/RowWiseAndColumnWiseSortedMatrix/FindElementInRowWiseAndColumnWiseSortedMatrix.java
@@ -0,0 +1,13 @@
+package matrix.RowWiseAndColumnWiseSortedMatrix;
+
+/*
+ * We have given a matrix of row and column wise sorted. The task is to find out the element in that matrix. If present, return "found" if not return "Not Found"
+ */
+
+public class FindElementInRowWiseAndColumnWiseSortedMatrix {
+
+    public static void main(String args[]){
+        int mat[][] = {{},{},{},{}};
+    }
+    
+}
