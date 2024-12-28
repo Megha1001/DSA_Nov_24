1db3ed8684b9cd7a4e4f7a75f2ad94b0369d57ae
Megha Verma <meghaverma1001@gmail.com>
2024-12-28 13:07:12 +0530

    Create PrintMatrixInSpiralPattern.java

---
+++ matrix/spiralPattern/PrintMatrixInSpiralPattern.java
@@ -0,0 +1,12 @@
+package matrix.spiralPattern;
+
+public class PrintMatrixInSpiralPattern {
+
+    public static void main(String args[]){
+        int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+
+        System.out.println("Elements of matrix after printing in spiral form are : ");
+        printSpiralForm(arr);
+    }
+    
+}
