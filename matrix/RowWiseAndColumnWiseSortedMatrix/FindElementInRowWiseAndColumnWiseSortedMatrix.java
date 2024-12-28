3990d6c7502ac53f18b15a7958c106a26dd0d510
Megha Verma <meghaverma1001@gmail.com>
2024-12-28 18:13:50 +0530

    Update FindElementInRowWiseAndColumnWiseSortedMatrix.java

--- matrix/RowWiseAndColumnWiseSortedMatrix/FindElementInRowWiseAndColumnWiseSortedMatrix.java
+++ matrix/RowWiseAndColumnWiseSortedMatrix/FindElementInRowWiseAndColumnWiseSortedMatrix.java
@@ -2,12 +2,24 @@ package matrix.RowWiseAndColumnWiseSortedMatrix;
 
 /*
  * We have given a matrix of row and column wise sorted. The task is to find out the element in that matrix. If present, return "found" if not return "Not Found"
+ * 
+ * Idea : 
+ *  The can be solved by two ways
+ *  1. Considering top-right element as start
+ *         ->if(top-right == x) return
+ *         -> else if top-right > x then go to left
+ *         -> else go to right
+ *  2. Considering bottom left element as star
+ *         ->if(bottom-left == x) return
+ *         -> else if bottom-left > x then go up
+ *         -> else go down
  */
 
 public class FindElementInRowWiseAndColumnWiseSortedMatrix {
 
     public static void main(String args[]){
-        int mat[][] = {{},{},{},{}};
+        int mat[][] = {{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
+        int x = 29;
     }
     
 }
