beae044fd3c665a5cc3277732bcd6a9abee56e09
Megha Verma <meghaverma1001@gmail.com>
2024-12-28 09:20:21 +0530

    Update PrintBoundaryTraversalOfMatrix.java

--- matrix/matrixBoundaryTraversal/PrintBoundaryTraversalOfMatrix.java
+++ matrix/matrixBoundaryTraversal/PrintBoundaryTraversalOfMatrix.java
@@ -8,13 +8,25 @@ public class PrintBoundaryTraversalOfMatrix {
 
     public static void main(String args[]){
 
+        // int arr[][] = {
+        //             {1,2,3,4},
+        //             {5,6,7,8},
+        //             {9,10,11,12},
+        //             {13,14,15,16}
+                // };
         int arr[][] = {
-                    {1,2,3,4},
-                    {5,6,7,8},
-                    {9,10,11,12},
-                    {13,14,15,16}
-                };
+            {1},
+            {5},
+            {9},
+            {13}
+        };
         
+        /* int arr[][] = {
+            {1,2,3,4},
+            // {5,6,7,8},
+            // {9,10,11,12},
+            // {13,14,15,16}
+        }; */      
         System.out.println("The boundary traversal of given matrix is :");
         printBoundaryTraversal(arr);
 
@@ -31,26 +43,41 @@ public class PrintBoundaryTraversalOfMatrix {
          * first column (bottom to Top )
          */
 
-
-        //first row -> L to R
-        for(int i = 0; i < arr[0].length; i++){
-            System.out.print(arr[0][i]+ " ");
+        //special handling for 1 row or 1 column
+        if(row==1){
+            for(int i=0; i<arr[0].length; i++){
+                System.out.print(arr[0][i]+ " ");
+            }
         }
 
 
-        //last column -> top to bottom
-        for(int i = 1; i < row; i++){
-            System.out.print(arr[i][row-1]+ " ");
+        else if(arr[0].length==1){
+            for(int i=0; i<row; i++){
+                System.out.print(arr[i][0]+ " ");
+            }
         }
 
-        //last row -> R to L
-        for(int i = arr[row-1].length -2; i>=0; i--){
-            System.out.print(arr[row-1][i]+ " ");
-        }
+        else{
+            //first row -> L to R
+            for(int i = 0; i < arr[0].length; i++){
+                System.out.print(arr[0][i]+ " ");
+            }
 
-        //first column -> bottom to top
-        for(int i = row-2; i>=1; i--){
-            System.out.print(arr[i][0]+" ");
+
+            //last column -> top to bottom
+            for(int i = 1; i < row; i++){
+                System.out.print(arr[i][row-1]+ " ");
+            }
+
+            //last row -> R to L
+            for(int i = arr[row-1].length -2; i>=0; i--){
+                System.out.print(arr[row-1][i]+ " ");
+            }
+
+            //first column -> bottom to top
+            for(int i = row-2; i>=1; i--){
+                System.out.print(arr[i][0]+" ");
+            }
         }
 
     }
