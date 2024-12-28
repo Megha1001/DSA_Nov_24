b6ef7836192df2be31fb0bd047b133298e2fa72a
Megha Verma <meghaverma1001@gmail.com>
2024-12-28 10:43:26 +0530

    Create PrintTransposeOfMatrixUsingEfficientApproach.java

---
+++ matrix/transposeOfMatrix/PrintTransposeOfMatrixUsingEfficientApproach.java
@@ -0,0 +1,42 @@
+package matrix.transposeOfMatrix;
+
+
+
+public class PrintTransposeOfMatrixUsingEfficientApproach {
+    
+    public static void main(String args[]){
+        int arr[][] = {
+                        {1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}
+                    };
+        
+        System.out.println("Transpose of the matrix is : ");
+        printTranspose(arr);
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+
+    public static void printTranspose(int arr[][]){
+
+        int n = arr.length; //Assuming, its a square matrix
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                //swap
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+    }
+}
