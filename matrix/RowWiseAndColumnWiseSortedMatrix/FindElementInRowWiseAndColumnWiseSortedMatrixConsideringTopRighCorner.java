ed8d877f16688dce1a9a563fdd456562c3c89e20
Megha Verma <meghaverma1001@gmail.com>
2024-12-28 18:24:07 +0530

    Update FindElementInRowWiseAndColumnWiseSortedMatrixConsideringTopRighCorner.java

--- matrix/RowWiseAndColumnWiseSortedMatrix/FindElementInRowWiseAndColumnWiseSortedMatrixConsideringTopRighCorner.java
+++ matrix/RowWiseAndColumnWiseSortedMatrix/FindElementInRowWiseAndColumnWiseSortedMatrixConsideringTopRighCorner.java
@@ -15,7 +15,7 @@ package matrix.RowWiseAndColumnWiseSortedMatrix;
  *         -> else go right
  */
 
-public class FindElementInRowWiseAndColumnWiseSortedMatrix {
+public class FindElementInRowWiseAndColumnWiseSortedMatrixConsideringTopRighCorner {
 
     public static void main(String args[]){
         int mat[][] = {{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
