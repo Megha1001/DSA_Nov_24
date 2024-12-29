fa547efa56c8b789ba9588037b608b0ec094fb24
Megha Verma <meghaverma1001@gmail.com>
2024-12-29 20:26:44 +0530

    Update CountDistinctInTwoUnsortedArraysCombined.java

--- hashing/unionOfTwoUnsortedArrays/CountDistinctInTwoUnsortedArraysCombined.java
+++ hashing/unionOfTwoUnsortedArrays/CountDistinctInTwoUnsortedArraysCombined.java
@@ -1,5 +1,7 @@
 package hashing.unionOfTwoUnsortedArrays;
 
+import java.util.HashSet;
+
 public class CountDistinctInTwoUnsortedArraysCombined {
 
 
@@ -10,4 +12,24 @@ public class CountDistinctInTwoUnsortedArraysCombined {
         System.out.println("Number of distinct elements in given array combined is : "+countDistinct(a, b, a.length, b.length));
     }
     
+
+    public static int countDistinct(int a[], int b[], int n1, int n2){
+
+        //AUXILIARY SPACE : O(N1+N2)
+        HashSet<Integer> set = new HashSet<>();
+
+        // Time Complexity : Theta(N1)
+        for(int i=0; i<n1; i++){
+            set.add(a[i]);
+        }
+
+        // Time Complexity : Theta(N2)
+        for(int i=0; i<n2; i++){
+            set.add(b[i]);
+        }
+
+        return set.size();
+
+    }
+
 }
