6ea4c7c8492041d47147b0d0724efa755aa2c728
Megha Verma <meghaverma1001@gmail.com>
2024-12-29 15:03:14 +0530

    Update CountDistinctElementsUsingHashSetImprovedInTermsOfWritingNotComplexity.java

--- hashing/countDistinctElements/CountDistinctElementsUsingHashSetImprovedInTermsOfWritingNotComplexity.java
+++ hashing/countDistinctElements/CountDistinctElementsUsingHashSetImprovedInTermsOfWritingNotComplexity.java
@@ -13,6 +13,7 @@ public class CountDistinctElementsUsingHashSetImprovedInTermsOfWritingNotComplex
     public static void main(String args[]){
         Integer arr[] = {15,12,13,12,13,13,18}; // note type of data its Integer not int
         HashSet<Integer> set = new HashSet<>(Arrays.asList(arr));
+        System.out.println("Number of distinct elements are : "+set.size());
     }
 
     
