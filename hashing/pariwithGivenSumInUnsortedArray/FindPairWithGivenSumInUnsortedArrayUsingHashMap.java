3a7ce39b218022f9a9568b0a2ec5616d6b3be69c
Megha Verma <meghaverma1001@gmail.com>
2024-12-29 20:44:46 +0530

    Create FindPairWithGivenSumInUnsortedArrayUsingHashMap.java

---
+++ hashing/pariwithGivenSumInUnsortedArray/FindPairWithGivenSumInUnsortedArrayUsingHashMap.java
@@ -0,0 +1,31 @@
+package hashing.pariwithGivenSumInUnsortedArray;
+
+/*
+ * TIME COMPLEXITY : O(N*N)
+ * AUXILIARY SPACE : O(1)
+ */
+
+public class FindPairWithGivenSumInUnsortedArrayUsingHashMap {
+
+    public static void main(String args[]){
+        int arr[] = {3,2,8,15,-8};
+        int x = 17;
+
+        System.out.println("Pair with given sum exists in given array ? "+checkIfSumExists(arr, arr.length, x));
+    }
+
+
+    public static boolean checkIfSumExists(int arr[], int n, int x){
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i]+arr[j]==x){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    
+}
