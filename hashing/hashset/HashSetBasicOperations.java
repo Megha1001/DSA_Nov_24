33b2b9132506c2c817772d3d493650a2ba2b516f
Megha Verma <meghaverma1001@gmail.com>
2024-12-29 13:40:48 +0530

    Update HashSetBasicOperations.java

--- hashing/hashset/HashSetBasicOperations.java
+++ hashing/hashset/HashSetBasicOperations.java
@@ -1,12 +1,23 @@
 package hashing.hashset;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class HashSetBasicOperations {
     
 
     public static void main(String args[]){
         HashSet<String> s = new HashSet<>();
+
+        s.add("abc");
+        
+        Iterator<String> iterator = s.iterator();
+
+        while(iterator.hasNext()){
+           System.out.print(iterator.next()+" "); 
+        }
+
+
     }
 
 }
