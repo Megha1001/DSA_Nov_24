e102103f196bcf90e8be04a944d1524712d4df90
Megha Verma <meghaverma1001@gmail.com>
2024-12-29 20:42:58 +0530

    Create FindPairWithGivenSumInUnsortedArrayUsingNaiveApproach.java

---
+++ hashing/pariwithGivenSumInUnsortedArray/FindPairWithGivenSumInUnsortedArrayUsingNaiveApproach.java
@@ -0,0 +1,26 @@
+package hashing.pariwithGivenSumInUnsortedArray;
+
+public class FindPairWithGivenSumInUnsortedArrayUsingNaiveApproach {
+
+    public static void main(String args[]){
+        int arr[] = {3,2,8,15,-8};
+        int x = 17;
+
+        System.out.println("Pair with given sum exists in given array ? "+checkIfSumExists(arr, arr.length, x));
+    }
+
+
+    public static boolean checkIfSumExists(int arr[], int n, int x){
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i]+arr[j]==x){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    
+}
