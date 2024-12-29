bc800cdf0f5c193d66857dc1c551dff401507969
Megha Verma <meghaverma1001@gmail.com>
2024-12-29 21:01:11 +0530

    Update FindPairWithGivenSumInUnsortedArrayUsingHashMap.java

--- hashing/pariwithGivenSumInUnsortedArray/FindPairWithGivenSumInUnsortedArrayUsingHashMap.java
+++ hashing/pariwithGivenSumInUnsortedArray/FindPairWithGivenSumInUnsortedArrayUsingHashMap.java
@@ -12,8 +12,10 @@ import java.util.Map;
 public class FindPairWithGivenSumInUnsortedArrayUsingHashMap {
 
     public static void main(String args[]){
-        int arr[] = {3,2,8,15,-8};
-        int x = 17;
+        // int arr[] = {3,2,8,15,-8};
+        // int x = 17;
+        int arr[] = {2,1,6,3};
+        int x = 6;
 
         System.out.println("Pair with given sum exists in given array ? "+checkIfSumExists(arr, arr.length, x));
     }
@@ -27,9 +29,16 @@ public class FindPairWithGivenSumInUnsortedArrayUsingHashMap {
         }
 
 
+        System.out.println("map "+map);
+
         for(Map.Entry<Integer,Integer> e : map.entrySet()){
 
+            /*
+             * Need && condition since for example we are looking for sum=6 and map created with {2,1,6,3}; example is {1=5, 2=4, 3=3, 6=0} then 3=3 will get matched with itself
+             */
+
             if(map.containsKey(e.getValue())){
+                System.out.println("key : "+e.getKey()+"|value "+e.getValue());
                 return true;
             }
 
