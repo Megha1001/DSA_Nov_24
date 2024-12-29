4918e259d15855d181e7fd629665ac55a12e758c
Megha Verma <meghaverma1001@gmail.com>
2024-12-29 15:07:44 +0530

    Update CountDistinctElementsUsingHashSetImprovedInTermsOfWritingNotComplexity.java

--- hashing/countDistinctElements/CountDistinctElementsUsingHashSetImprovedInTermsOfWritingNotComplexity.java
+++ hashing/countDistinctElements/CountDistinctElementsUsingHashSetImprovedInTermsOfWritingNotComplexity.java
@@ -6,6 +6,9 @@ import java.util.Arrays;
 /*
  * TIME COMPLEXIY : Theta(N)
  * AUXILIARY SPACE : O(N) --> *****NOTE THAT*****
+ * why TC : Theta(N) ? because before insertion we are checking it in . if not present then we are inserting it
+ * why Auxiliary space : O(N) ? 
+    Hashset can contain 1 item(if all elements are same in given array) or may contain all the items(if all are distinct in given array)
  */
 
 public class CountDistinctElementsUsingHashSetImprovedInTermsOfWritingNotComplexity {
