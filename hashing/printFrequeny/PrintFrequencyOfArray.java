7749c66810ced087359ef62618e180a3ded4c86f
Megha Verma <meghaverma1001@gmail.com>
2024-12-29 16:07:52 +0530

    Create PrintFrequencyOfArray.java

---
+++ hashing/printFrequeny/PrintFrequencyOfArray.java
@@ -0,0 +1,25 @@
+package hashing.printFrequeny;
+
+import java.util.HashMap;
+
+public class PrintFrequencyOfArray {
+    
+    public static void main(String args[]){
+        int arr[] = {10,12,10,15,10,20,12,12};
+
+        System.out.println("Frequency are as follows");
+        printFreq(arr, arr.length);
+    }
+
+    public static void printFreq(int arr[], int n){
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        }
+
+        System.out.println("map : "+map);
+
+    }
+
+}
