d2d13f192768fdb07ee51936f8f4e60ee423d021
Megha Verma <meghaverma1001@gmail.com>
2024-12-29 21:20:12 +0530

    Update FindPairWithGivenSumInUnsortedArrayUsingHashSet.java

--- hashing/pariwithGivenSumInUnsortedArray/FindPairWithGivenSumInUnsortedArrayUsingHashSet.java
+++ hashing/pariwithGivenSumInUnsortedArray/FindPairWithGivenSumInUnsortedArrayUsingHashSet.java
@@ -9,11 +9,16 @@ public class FindPairWithGivenSumInUnsortedArrayUsingHashSet {
         int arr[] = {3, 3, 4, 2}; 
         int x = 6;
 
-        System.out.println("Pair with given sum exists in given array ? "+checkIfSumExists(arr, arr.length, x));
+        System.out.println("Pair with given sum exists in given array ? "+checkIfSumExists(arr, x));
     }
     
 
-    public static boolean checkIfSumExists(int arr, int n, int x){
+    public static boolean checkIfSumExists(int arr, int x){
+        HashSet<Integer> complements = new HashSet<>();
+
+        for(int num : arr){
+
+        }
 
 
         return false;
