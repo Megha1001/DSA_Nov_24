4380d0a178682ed3166b589ca5d67ac2989cb654
Megha Verma <meghaverma1001@gmail.com>
2024-12-29 15:00:20 +0530

    Create CountDistinctElementsUsingHashSetImprovedInTermsOfWritingNotComplexity.java

---
+++ hashing/countDistinctElements/CountDistinctElementsUsingHashSetImprovedInTermsOfWritingNotComplexity.java
@@ -0,0 +1,28 @@
+package hashing.countDistinctElements;
+
+import java.util.HashSet;
+
+/*
+ * TIME COMPLEXIY : Theta(N)
+ * AUXILIARY SPACE : O(N) --> *****NOTE THAT*****
+ */
+
+public class CountDistinctElementsUsingHashSetImprovedInTermsOfWritingNotComplexity {
+    
+    public static void main(String args[]){
+        int arr[] = {15,12,13,12,13,13,18};
+
+        System.out.println("Number of distinct elements in given array is : "+countDistinct(arr));
+    }
+
+    public static int countDistinct(int arr[]){
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i : arr){
+            set.add(i);
+        }
+
+        return set.size();
+    }
+    
+}
