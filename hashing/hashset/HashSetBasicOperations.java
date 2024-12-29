113943e4933d3ca078d12902487872ddcdb79640
Megha Verma <meghaverma1001@gmail.com>
2024-12-29 13:38:58 +0530

    Create HashSetBasicOperations.java

---
+++ hashing/hashset/HashSetBasicOperations.java
@@ -0,0 +1,12 @@
+package hashing.hashset;
+
+import java.util.HashSet;
+
+public class HashSetBasicOperations {
+    
+
+    public static void main(String args[]){
+        HashSet<String> s = new HashSet<>();
+    }
+
+}
