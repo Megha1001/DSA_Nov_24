7a37eb87a35928fc332e3528b1f267e94914356b
Megha Verma <meghaverma1001@gmail.com>
2024-12-30 21:47:00 +0530

    Create FindLongestCommonSpanWithSameSumInBinaryArraysUsingNaiveApproach.java

---
+++ hashing/longestCommonSpanWithSameSumInBinaryArrays/FindLongestCommonSpanWithSameSumInBinaryArraysUsingNaiveApproach.java
@@ -0,0 +1,9 @@
+package hashing.longestCommonSpanWithSameSumInBinaryArrays;
+
+/*
+ * Question: Find out the length of longest common span with equal sum and same starting and ending index and same length.
+ */
+
+public class FindLongestCommonSpanWithSameSumInBinaryArraysUsingNaiveApproach {
+    
+}
