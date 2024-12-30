ba3014edde2328dcf20b77c22db7771282e2cd2d
Megha Verma <meghaverma1001@gmail.com>
2024-12-30 10:50:22 +0530

    Create CheckSubArrayWithSumZeroExistsUsingHashingEfficientApproach.java

---
+++ hashing/subArrayWithSumZero/CheckSubArrayWithSumZeroExistsUsingHashingEfficientApproach.java
@@ -0,0 +1,39 @@
+package hashing.subArrayWithSumZero;
+
+/*
+ * LOGIC : Use Prefix Sum and hashing
+ * 
+ * a0, a1, a2, ...., ai-1, ai, ai+1, ......, aj......an-1
+ * ai, ai+1, ......, aj ==> Sum is zero
+ * 
+ * Then , 
+ *  
+ * Sum(a0, a1, a2, ...., ai-1, ai, ai+1, ......, aj) == Sum(a0, a1, a2, ...., ai-1)
+ * 
+ * 
+ * Do prefix sum and store in HashSet, if we encounter same prefix sum again that means we got the subarray with sum zero
+ * It will handle elments with value zero as well.
+ *
+ * 
+ * TIME COMPLEXITY : O(N)
+ * AUXILIARY SPACE : O(N)
+ */
+
+public class CheckSubArrayWithSumZeroExistsUsingHashingEfficientApproach {
+
+    public static void main(String args[]){
+
+        int arr[] = {-3, 4, -3, -1, 1};
+        System.out.println("Does subarray with sum zero exists ? "+isSubArrayWithSumZeroExists(arr,arr.length));
+
+    }
+    
+
+    public static boolean isSubArrayWithSumZeroExists(int arr[], int n){
+
+        
+        return false;
+
+    }
+
+}
