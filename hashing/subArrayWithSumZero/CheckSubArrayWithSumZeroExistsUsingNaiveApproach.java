d84a0a490048619cb8c15d12f5c5a810f509f0d8
Megha Verma <meghaverma1001@gmail.com>
2024-12-30 10:45:42 +0530

    Create CheckSubArrayWithSumZeroExistsUsingNaiveApproach.java

---
+++ hashing/subArrayWithSumZero/CheckSubArrayWithSumZeroExistsUsingNaiveApproach.java
@@ -0,0 +1,12 @@
+package hashing.subArrayWithSumZero;
+
+public class CheckSubArrayWithSumZeroExistsUsingNaiveApproach {
+
+    public static void main(String args[]){
+
+        int arr[] = {-3, 4, -3, -1, 1};
+        System.out.println("Does subarray with sum zero exists ? "+isSubArrayWithSumZeroExists(arr,arr.length));
+
+    }
+    
+}
