5a02666639d900a1bb4a21535296e918612efd77
Megha Verma <meghaverma1001@gmail.com>
2024-12-30 20:03:44 +0530

    Update FindLongestSubArrayWithEqualNumberOfZeroAndOneUsingNaiveSolution.java

--- hashing/longestSubArrayWithEqualNumberOf0And1/FindLongestSubArrayWithEqualNumberOfZeroAndOneUsingNaiveSolution.java
+++ hashing/longestSubArrayWithEqualNumberOf0And1/FindLongestSubArrayWithEqualNumberOfZeroAndOneUsingNaiveSolution.java
@@ -4,7 +4,9 @@ public class FindLongestSubArrayWithEqualNumberOfZeroAndOneUsingNaiveSolution {
 
     public static void main(String [] args){
         // int arr[] = {1, 0, 1, 1, 1, 0, 0};
-        int arr[] = {1, 0, 1, 1, 1, 0, 0};
+        // int arr[] = {1, 1, 1, 1, 1};
+        // int arr[] = {0,0,1,1,1,1,1,0};
+        int arr[] = {0,0,1,0,1,1};
         System.out.println("The longest subarray with equal number of 0's and 1's is : "+findLongestSubArray(arr, arr.length));
     }
 
