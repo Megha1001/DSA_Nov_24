342ea69e9b1a8db1a7f2e6d199a8d3bfcd8e97a1
Megha Verma <meghaverma1001@gmail.com>
2024-12-30 18:36:10 +0530

    Create FindLongestSubArrayWithGivenSumUsingHashing.java

---
+++ hashing/longestSubArrayWithGivenSum/FindLongestSubArrayWithGivenSumUsingHashing.java
@@ -0,0 +1,45 @@
+package hashing.longestSubArrayWithGivenSum;
+
+import java.util.HashMap;
+
+/*
+ * TIME COMPLEXITY : O(N)
+ * AUXILIARY SPACE : O(N)
+ */
+
+public class FindLongestSubArrayWithGivenSumUsingHashing {
+
+    public static void main(String args[]){
+        int arr[] = {5,8,-4,-4,9,-2,2};
+        int x = 0;
+
+        System.out.println("The longest subarray with given sum is : "+findLongestSubArrayLengthWithGivenSum(arr, arr.length,x));
+    }
+
+
+    public static int findLongestSubArrayLengthWithGivenSum(int arr[], int n, int x){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res=0;
+        int sum=0;
+
+
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+
+            if(sum==x){
+                res = Math.max(res, i); //it will always be a prefix sum
+            } else if( map.getOrDefault( sum-x, -1) != -1){
+                int lastIdx = map.get(sum-x);
+                res = Math.max(res, i-lastIdx);
+            }
+
+            map.put(sum, i);
+
+        }
+
+    
+        return res;
+
+    }
+    
+}
