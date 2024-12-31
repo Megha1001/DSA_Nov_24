73aae0a3a1e9ddf6996bd08537dcbe5f67f4ad0a
Megha Verma <meghaverma1001@gmail.com>
2024-12-31 18:16:02 +0530

    Update CountDistinctElmentsInEveryWindowUsingHashingEfficientApproach.java

--- hashing/countDistinctElementsInEveryWindow/CountDistinctElmentsInEveryWindowUsingHashingEfficientApproach.java
+++ hashing/countDistinctElementsInEveryWindow/CountDistinctElmentsInEveryWindowUsingHashingEfficientApproach.java
@@ -1,10 +1,20 @@
 package hashing.countDistinctElementsInEveryWindow;
 
-import java.util.HashSet;
+import java.util.HashMap;
 
 /*
- * TIME COMPLEXITY : Theta((n-k)*k)
- * AUXILIARY SPACE : O(k)
+ * LOGIC : Use Sliding WIndow technique.
+ *  Input : {10,20,10,10,30,40}, k=4
+ * 1. Create a frequency map of first K elements
+ *  freq{10} = 3, freq{20} = 1
+ * 
+ * 2. print the size of map : 2
+ * 3. for(int i=k; i<n; i++)
+ *      Decrease the frequence of arr[i-k]
+ *      If the frequency of arr[i-k] becomes zero, remove it from map
+ *      If arr[i] doesn't exists in the map , insert it. Else increase its frequency by 1
+ *      print the size of map.
+ * 
  */
 
 
