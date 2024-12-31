01d30700a56a405c761fc803aca34acc7b35fe20
Megha Verma <meghaverma1001@gmail.com>
2024-12-31 11:02:32 +0530

    Update FindLongestCommonSpanWithSameSumInBinaryArrayUsingHashing.java

--- hashing/longestCommonSpanWithSameSumInBinaryArrays/FindLongestCommonSpanWithSameSumInBinaryArrayUsingHashing.java
+++ hashing/longestCommonSpanWithSameSumInBinaryArrays/FindLongestCommonSpanWithSameSumInBinaryArrayUsingHashing.java
@@ -2,8 +2,8 @@ package hashing.longestCommonSpanWithSameSumInBinaryArrays;
 
 public class FindLongestCommonSpanWithSameSumInBinaryArrayUsingHashing {
 
-    pulbic static void main(String args[]){
-        
+    public static void main(String args[]){
+
     }
     
 }
