caa015fd3fc410bcdf8c60414426af092f6e9235
Megha Verma <meghaverma1001@gmail.com>
2024-12-31 11:06:35 +0530

    Update FindLongestCommonSpanWithSameSumInBinaryArrayUsingHashing.java

--- hashing/longestCommonSpanWithSameSumInBinaryArrays/FindLongestCommonSpanWithSameSumInBinaryArrayUsingHashing.java
+++ hashing/longestCommonSpanWithSameSumInBinaryArrays/FindLongestCommonSpanWithSameSumInBinaryArrayUsingHashing.java
@@ -1,6 +1,20 @@
 package hashing.longestCommonSpanWithSameSumInBinaryArrays;
 
-
+/*
+ * LOGIC : Use Hashing and findLongestSubArrayWithSumZero solution
+ * 
+ * 1. Compute a difference array
+ *  a1[] = {0,1,0,0,0,0};
+ * a2[] = {1,0,1,0,0,1};
+ * temp[]= {-1,1,-1,0,0,-1};
+ * 
+ * 2.Return the length of the longest Subarray with 0 sum in temp
+ *  Values in temp
+ *      - We get 0 when values are same in both
+ *      - We get 1 when a1[i]=1 and a2[i]=0
+ *      - We get -1 when a1[i]=0 and a2[i]=1
+ * 
+ */
 
 public class FindLongestCommonSpanWithSameSumInBinaryArrayUsingHashing {
 
