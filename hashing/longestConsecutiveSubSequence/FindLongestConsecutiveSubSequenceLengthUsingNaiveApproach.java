3d2fbfc78a19dbf1bb29ff8ec528f525e1317a6d
Megha Verma <meghaverma1001@gmail.com>
2024-12-31 11:54:02 +0530

    Update FindLongestConsecutiveSubSequenceLengthUsingNaiveApproach.java

--- hashing/longestConsecutiveSubSequence/FindLongestConsecutiveSubSequenceLengthUsingNaiveApproach.java
+++ hashing/longestConsecutiveSubSequence/FindLongestConsecutiveSubSequenceLengthUsingNaiveApproach.java
@@ -5,16 +5,16 @@ import java.util.Arrays;
 /*
  * Question : We need to find the longest subsequence in the form of x, x+1, x+2, ...., x+i with these element appearing in ANY order
  * 
- * TIME COMPLEXITY : Theta(N*N)
+ * TIME COMPLEXITY : Theta(NlogN)
  * AUXILIARY SPACE : O(1)
  */
 
 public class FindLongestConsecutiveSubSequenceLengthUsingNaiveApproach {
 
     public static void main(String args[]){
-        // int arr[] = {1,9,3,4,2,20};
+        int arr[] = {1,9,3,4,2,20};
         // int arr[] = {8,20,7,30};
-        int arr[] = {10,20,30};
+        // int arr[] = {10,20,30};
         
         System.out.println("The longest consecutive subsequence length in given array is : "+findLongestSubSequenceLengthWithConsecutiveElements(arr, arr.length));
     }
@@ -25,15 +25,24 @@ public class FindLongestConsecutiveSubSequenceLengthUsingNaiveApproach {
 
         Arrays.sort(arr);
 
-        for(int i=0; i<n; i++){
-            int count=1;
-            for(int j=i+1; j<n; j++){
-                if(arr[j]-arr[j-1]==1){
-                    ++count;
-                }else{
-                    count=1;
-                }
+        // for(int i=0; i<n; i++){
+        //     int count=1;
+        //     for(int j=i+1; j<n; j++){
+        //         if(arr[j]-arr[j-1]==1){
+        //             ++count;
+        //         }else{
+        //             count=1;
+        //         }
+        //         res = Math.max(res, count);
+        //     }
+        // }
+        int count = 1;
+        for(int i = 0; i < n - 1; i++){
+            if(arr[i+1]-arr[i]==1){
+                ++count;
                 res = Math.max(res, count);
+            }else{
+                count=1;
             }
         }
 
