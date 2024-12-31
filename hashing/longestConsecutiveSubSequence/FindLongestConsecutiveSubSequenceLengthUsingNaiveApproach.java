ccef723f3301ab89febcdad9b8f4831831b89405
Megha Verma <meghaverma1001@gmail.com>
2024-12-31 11:38:04 +0530

    Create FindLongestConsecutiveSubSequenceLengthUsingNaiveApproach.java

---
+++ hashing/longestConsecutiveSubSequence/FindLongestConsecutiveSubSequenceLengthUsingNaiveApproach.java
@@ -0,0 +1,9 @@
+package hashing.longestConsecutiveSubSequence;
+
+/*
+ * Quesiton : We need to find the longest subsequence in the form of x, x+1, x+2, ...., x+i with these element appearing in ANY order
+ */
+
+public class FindLongestConsecutiveSubSequenceLengthUsingNaiveApproach {
+    
+}
