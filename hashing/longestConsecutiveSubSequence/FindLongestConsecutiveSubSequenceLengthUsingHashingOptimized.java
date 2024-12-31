e9f0d43ec4ab7282d2ac49d82000deda38ca59e6
Megha Verma <meghaverma1001@gmail.com>
2024-12-31 12:53:03 +0530

    Create FindLongestConsecutiveSubSequenceLengthUsingHashingOptimized.java

---
+++ hashing/longestConsecutiveSubSequence/FindLongestConsecutiveSubSequenceLengthUsingHashingOptimized.java
@@ -0,0 +1,42 @@
+package hashing.longestConsecutiveSubSequence;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+/*
+ * Question : We need to find the longest subsequence in the form of x, x+1, x+2, ...., x+i with these element appearing in ANY order
+ * 
+ * TIME COMPLEXITY : O(N*K)
+ * AUXILIARY SPACE : O(N)
+ * 
+ * LOGICE : 1. We first insert all the elements in hash table(HashSet).
+ * 2. We do lookup to find the result
+ */
+
+public class FindLongestConsecutiveSubSequenceLengthUsingHashingOptimized {
+
+    public static void main(String args[]){
+
+        int arr[] = {1,3,4,3,3,2,9,10};
+        
+        System.out.println("The longest consecutive subsequence length in given array is : "+findLongestSubSequenceLengthWithConsecutiveElements(arr, arr.length));
+    }
+
+    public static int findLongestSubSequenceLengthWithConsecutiveElements(int arr[], int n){
+
+        HashSet<Integer> set = new HashSet<>();
+        int res=1;
+
+        // create set
+        for(int i : arr){
+            set.add(i);
+        }
+        
+
+
+
+        return res;
+
+    }
+    
+}
