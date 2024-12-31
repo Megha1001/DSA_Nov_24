0d6a58a1eec21e35b0c1e72d70ee7f3182c287c0
Megha Verma <meghaverma1001@gmail.com>
2024-12-31 11:46:31 +0530

    Update FindLongestConsecutiveSubSequenceLengthUsingNaiveApproach.java

--- hashing/longestConsecutiveSubSequence/FindLongestConsecutiveSubSequenceLengthUsingNaiveApproach.java
+++ hashing/longestConsecutiveSubSequence/FindLongestConsecutiveSubSequenceLengthUsingNaiveApproach.java
@@ -23,13 +23,13 @@ public class FindLongestConsecutiveSubSequenceLengthUsingNaiveApproach {
         for(int i=0; i<n; i++){
             int count=1;
             for(int j=i+1; j<n; j++){
-                if(arr[j]-arr[i]==1){
+                if(arr[j]-arr[j-1]==1){
                     ++count;
                 }else{
                     count=1;
                 }
+                res = Math.max(res, count);
             }
-            res = Math.max(res, count);
         }
 
         return res;
