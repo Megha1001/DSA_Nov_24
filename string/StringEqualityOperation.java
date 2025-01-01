00a2b71b56588eff3cea0d124c51c61f5d90f980
Megha Verma <meghaverma1001@gmail.com>
2025-01-01 19:39:04 +0530

    Update StringEqualityOperation.java

--- string/StringEqualityOperation.java
+++ string/StringEqualityOperation.java
@@ -3,7 +3,29 @@ package string;
 public class StringEqualityOperation {
 
     public static void main(String args[]){
-        
+
+        String s1 = "geeks";
+        String s2 = "geeks";
+
+        if(s1==s2){
+            System.out.println("s1 literal is equal to s2 ");
+        }else{
+            System.out.println("s1 literal is not equal to s2 ");
+        }
+
+
+        String s3 = new String("geeks");
+
+        if(s1==s3){ // reference comparision
+            System.out.println("s1 literal is equal to s3 ");
+        }else{
+            System.out.println("s1 literal is not equal to s3 ");
+        }
+
+        if(s1.equals(s3)){ //content comparision
+            System.out.println("s1 literal is equal to s3 using equals method");
+        }
+
     }
     
 }
