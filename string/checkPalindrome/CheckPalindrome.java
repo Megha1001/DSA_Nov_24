5acb676385bfdcebb05d218c8152a1ea728f406b
Megha Verma <meghaverma1001@gmail.com>
2025-01-01 20:17:41 +0530

    Update CheckPalindrome.java

--- string/checkPalindrome/CheckPalindrome.java
+++ string/checkPalindrome/CheckPalindrome.java
@@ -3,8 +3,20 @@ package string.checkPalindrome;
 public class CheckPalindrome {
     
     public static void main(String args[]){
-        String s = "ABCDCBA";
-        System.
+        String s = "ABCDCBAe";
+        System.out.println("Is given string palindrome ? "+checkPalindrome(s, s.length()));
+    
+    }
+
+    public static boolean checkPalindrome(String s, int n){
+        
+        for(int i=0; i<n; i++){
+            if(s.charAt(i)!= s.charAt(n-1-i)){
+                return false;
+            }
+        }
+
+        return true;
     }
     
 }
