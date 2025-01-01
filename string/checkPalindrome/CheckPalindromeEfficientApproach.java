a081c28942d7126e98c55f1484ca75b98fcb161c
Megha Verma <meghaverma1001@gmail.com>
2025-01-01 20:42:41 +0530

    Create CheckPalindromeEfficientApproach.java

---
+++ string/checkPalindrome/CheckPalindromeEfficientApproach.java
@@ -0,0 +1,27 @@
+package string.checkPalindrome;
+
+/*
+ * TIME COMPLEXITY : O(str.length)
+ * AUXILIARY SPACE : O(1)
+ */
+
+public class CheckPalindromeEfficientApproach {
+    
+    public static void main(String args[]){
+        String s = "ABCDCBAe";
+        System.out.println("Is given string palindrome ? "+checkPalindrome(s, s.length()));
+    
+    }
+
+    public static boolean checkPalindrome(String s, int n){
+        
+        for(int i=0; i<n; i++){
+            if(s.charAt(i)!= s.charAt(n-1-i)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+}
