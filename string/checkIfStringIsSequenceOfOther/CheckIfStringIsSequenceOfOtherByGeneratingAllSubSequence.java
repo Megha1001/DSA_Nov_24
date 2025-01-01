9205c1250a04f21c399a1b9194340d89b097ebf9
Megha Verma <meghaverma1001@gmail.com>
2025-01-01 20:51:34 +0530

    Update CheckIfStringIsSequenceOfOtherByGeneratingAllSubSequence.java

--- string/checkIfStringIsSequenceOfOther/CheckIfStringIsSequenceOfOtherByGeneratingAllSubSequence.java
+++ string/checkIfStringIsSequenceOfOther/CheckIfStringIsSequenceOfOtherByGeneratingAllSubSequence.java
@@ -2,8 +2,30 @@ package string.checkIfStringIsSequenceOfOther;
 
 /*
  * ABC : "", A, B, C, AB, AC, BC, ABC -> 2^n
+ * 
+ * LOGIC : Generate all the subsequence and check if other contains first
+ * 
  */
 
 public class CheckIfStringIsSequenceOfOtherByGeneratingAllSubSequence {
+
+    public static void main(String args[]){
+        String s1 = "ABCD";
+        String s2 = "AD"; //true
+
+        System.out.println("does s2 is subsequence of s1 ? "+checkIfSubsequence(s1, s2));
+    }
+
+
+    public static boolean checkIfSubsequence(String s1, String s2){
+
+        return generateAllSubSequenceAndCheck(s1, "", 0, s2);
+    }
+
+    public static boolean generateAllSubSequenceAndCheck(String input, String curr, int k, String s2){
+        
+
+        return false;
+    }
     
 }
