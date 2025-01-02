3d3feb69a420845b6c0b079c2fe8d09772e2ec4c
Megha Verma <meghaverma1001@gmail.com>
2025-01-02 22:08:24 +0530

    Create CheckWhetherGivenStringsAreAnagramsUsingUsingArrayOptimized.java

---
+++ string/checkAnagrams/CheckWhetherGivenStringsAreAnagramsUsingUsingArrayOptimized.java
@@ -0,0 +1,20 @@
+package string.checkAnagrams;
+
+public class CheckWhetherGivenStringsAreAnagramsUsingUsingArrayOptimized {
+
+
+    public static void main(String args[]){
+        String s1 = "listen";
+        String s2 = "silent";
+
+        System.out.println("are given strings anagrams ? "+checkAnagram(s1, s2, s1.length(), s2.length()));
+    }
+
+
+    public static boolean checkAnagram(String s1, String s2, int n1, int n2){
+        
+        
+
+    }
+    
+}
