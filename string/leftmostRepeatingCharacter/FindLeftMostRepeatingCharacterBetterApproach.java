e6c7c341ad694c2b16fbc7b9e1b526018338fb26
Megha Verma <meghaverma1001@gmail.com>
2025-01-02 22:46:28 +0530

    Update FindLeftMostRepeatingCharacterBetterApproach.java

--- string/leftmostRepeatingCharacter/FindLeftMostRepeatingCharacterBetterApproach.java
+++ string/leftmostRepeatingCharacter/FindLeftMostRepeatingCharacterBetterApproach.java
@@ -2,6 +2,29 @@ package string.leftmostRepeatingCharacter;
 
 public class FindLeftMostRepeatingCharacterBetterApproach {
 
-    public static void main
+    private static int CHAR = 256;
+
+    public static void main(String args[]){
+        String s = "abccbd";
+        System.out.println("The leftmost repeating character in given string is : "+(char)findLeftMostRepeating(s, s.length()));
+    }
+
+    public static int findLeftMostRepeating(String s, int n){
+
+        int count[] = new int[CHAR];
+
+        for(int i=0; i<n; i++){
+            count[s.charAt(i)]++;
+        }
+
+        for(int i=0; i<n; i++){
+            if(count[s.charAt(i)] > 1){
+                return s.charAt(i);
+            }
+        }
+
+
+        return -1; // not present
+    }
     
 }
