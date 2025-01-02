83e0093884a70b6d6f87a2322391c7f85ac337e2
Megha Verma <meghaverma1001@gmail.com>
2025-01-02 20:18:19 +0530

    Update CheckIfStringIsSequenceOfOtherEfficientMethod.java

--- string/checkIfStringIsSequenceOfOther/CheckIfStringIsSequenceOfOtherEfficientMethod.java
+++ string/checkIfStringIsSequenceOfOther/CheckIfStringIsSequenceOfOtherEfficientMethod.java
@@ -1,5 +1,22 @@
 package string.checkIfStringIsSequenceOfOther;
 
+
+
+
 public class CheckIfStringIsSequenceOfOtherEfficientMethod {
+
+    public static void main(String args[]){
+        // String s1 = "ABCD";
+        // String s2 = "AD"; //true
+        String s1 = "ABCDE";
+        String s2 = "AED"; //false
+
+        System.out.println("does s2 is subsequence of s1 ? "+checkIfSubsequence(s1, s2));
+    }
+
+
+    public static boolean checkIfSubsequence(String s1, String s2){
+
+    }
     
 }
