9d9351aece140ba7092d64de940c546c7901a455
Megha Verma <meghaverma1001@gmail.com>
2025-01-02 21:34:36 +0530

    Update CheckWhetherGivenStringsAreAnagramsUsingUsingArray.java

--- string/checkAnagrams/CheckWhetherGivenStringsAreAnagramsUsingUsingArray.java
+++ string/checkAnagrams/CheckWhetherGivenStringsAreAnagramsUsingUsingArray.java
@@ -1,5 +1,18 @@
 package string.checkAnagrams;
 
 public class CheckWhetherGivenStringsAreAnagramsUsingUsingArray {
+
+
+    public static void main(String args[]){
+        String s1 = "listen";
+        String s2 = "silent";
+
+        System.out.println("are given strings anagrams ? "+checkAnagram(s1, s2, s1.length(), s2.length()));
+    }
+
+
+    public static boolean checkAnagram(String s1, String s2, int n1, int n2){
+        
+    }
     
 }
