e727fa8d73d21bd27b8983df74921fb17fc6c9b2
Megha Verma <meghaverma1001@gmail.com>
2025-01-04 16:20:31 +0530

    Create FindLeftMostRepeatingCharacterUsingVistedArray.java

---
+++ string/leftmostRepeatingCharacter/FindLeftMostRepeatingCharacterUsingVistedArray.java
@@ -0,0 +1,10 @@
+package string.leftmostRepeatingCharacter;
+
+public class FindLeftMostRepeatingCharacterUsingVistedArray {
+
+    public static void main(String args[]){
+        String s = "abccd";
+        System.out.println("The leftmost repeating character is : "+(char)findLeftMostRepeatingChar(s));
+    }
+    
+}
