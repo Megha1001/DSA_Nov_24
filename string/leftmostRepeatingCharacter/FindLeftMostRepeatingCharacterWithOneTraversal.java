996a7b8b85b9d0a183457f0ed449bee1ece0298f
Megha Verma <meghaverma1001@gmail.com>
2025-01-04 17:49:49 +0530

    Update FindLeftMostRepeatingCharacterWithOneTraversal.java

--- string/leftmostRepeatingCharacter/FindLeftMostRepeatingCharacterWithOneTraversal.java
+++ string/leftmostRepeatingCharacter/FindLeftMostRepeatingCharacterWithOneTraversal.java
@@ -8,19 +8,18 @@ package string.leftmostRepeatingCharacter;
 public class FindLeftMostRepeatingCharacterWithOneTraversal {
 
     public static void main(String args[]){
-        String s = "abccdb";
+        String s = "abcbc";
         System.out.println("The leftmost repeating character is : "+(char)findLeftMostRepeatingChar(s));
     }
 
     public static int findLeftMostRepeatingChar(String s){
 
         int count[] = new int[256];
-        int res = -1;
-
+        int res = Integer.MAX_VALUE;
         for(int i=0; i<s.length(); i++){
             int curr = count[s.charAt(i)];
             if(curr==1){
-                res= s.charAt(i); // we should assign this to variable otherwise it will return c.
+                res= Math.min(s.charAt(i), res); // we should assign this to variable otherwise it will return c.
             } else{
                 count[s.charAt(i)] = 1;
             }
