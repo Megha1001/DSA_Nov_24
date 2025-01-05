f36f2ff4b736567655168b8d07b49bd7f48175aa
Megha Verma <meghaverma1001@gmail.com>
2025-01-05 20:17:39 +0530

    Create ReverseWordsInStringNaiveApproach.java

---
+++ string/reverseWordsInAString/ReverseWordsInStringNaiveApproach.java
@@ -0,0 +1,19 @@
+package string.reverseWordsInAString;
+
+public class ReverseWordsInStringNaiveApproach {
+
+    public static void main(String args[]){
+
+        String input = "I am megha Verma.";
+        String [] characters = input.split(" ");
+
+        StringBuilder res = new StringBuilder();
+        for(int i=0; i<characters.length; i++){
+            res.append(new StringBuilder(characters[i]).reverse().append(" "));
+        }
+
+        System.out.println(res);
+    }
+    
+}
+
